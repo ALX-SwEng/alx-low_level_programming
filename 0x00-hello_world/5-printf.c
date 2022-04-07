2deb15f531d79957f698238daa871a10bddbb8b3
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-07 23:10:41 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,6 +2,6 @@
 #include <stdio.h>
 int main(void)
 {
-printf("ith proper grammar, but the outcome is a piece of art,");  
+printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
