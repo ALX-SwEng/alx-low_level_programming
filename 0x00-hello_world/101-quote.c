074134d46bf1ad9ad0821ead7652ca31fab5d93a
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-07 23:40:44 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,10 @@
-
+/**
+ * main - Print text to standard error output.
+ * Return: program returns 0
+ */
+#include <stdio.h>
+int main(void)
+{
+fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60, 1, stderr);
+return (0);
+}
