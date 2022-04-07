c2eea88b75b776c292a7a460b44ab6c2da6deee4
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-07 22:54:09 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,10 @@
-
+#include <stdio.h>
+int main(void)
+{
+printf("size of char = %d bytes\n", sizeof(char));
+printf("size of int = %d bytes\n", sizeof(int));
+printf("size of long = %d bytes\n", sizeof(long));
+printf("size of long long = %d bytes\n", sizeof(long long));
+printf("size of float = %d bytes\n", sizeof(float));
+return (0);
+}
