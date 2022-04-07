b60552a304e5d1433ac0fd250d694e01866f043d
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-07 23:49:44 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 int main(void)
 {
-fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
-       60, 1, stderr);
+fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+                                                        60, 1, stderr);
 return (1);
 }
