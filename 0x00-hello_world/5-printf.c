b84ddc4239b11fb9121f6b87551035552940ec5b
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-07 23:03:10 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
+//Print string to standard output using printf.
 #include <stdio.h>
 int main(void)
 {
- printf("ith proper grammar, but the outcome is a piece of art,");  
- return (0);
+printf("ith proper grammar, but the outcome is a piece of art,");  
+return (0);
 }
