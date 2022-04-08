575a9aa21969f6a81f026d1cdebcb0d010224172
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-08 22:38:20 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,10 +12,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%d is positive");
+		printf("%d is positive", n);
 	else if (n == 0)
-		printf("%d is zero");
+		printf("%d is zero", n);
 	else
-		printf("%d is negative");
+		printf("%d is negative", n);
 	return (0);
 }
