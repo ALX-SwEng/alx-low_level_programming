529ba8e761798de4bba036d0ae960ef17993890b
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-08 23:20:04 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,22 @@
-#include <stdlib.h>
-#include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+* main - print lowercases alphabet 
+* followed by uppercase alphabets
+* Return: program returns 0
+*/
 int main(void)
 {
-	
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c == 'e' || c == 'q')
+			continue;
+		putchar(c);
+		c++;
+	}	
+	putchar('\n');
 	return (0);
 }
