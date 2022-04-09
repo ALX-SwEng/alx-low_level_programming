da5d02af7bae6d113c6dab2f33d71c7ec6c97400
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-09 21:36:34 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,10 +1,35 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include <stdio.h>
+/**
+* main - print combination of single digits
+* Return: program returns 0
+*/
 int main(void)
 {
-	
+	int n = 0;
+
+	while (n <= 9)
+	{
+		int i = n;
+
+		while (i <= 9)
+		{
+			int j = i;
+			while (j <= 9)
+			{
+				if (n != i && i != j)
+				{
+					putchar (n % 10 + '0');
+					putchar (i % 10 + '0');
+					putchar (j % 10 + '0');
+					putchar (',');
+					putchar (' ');
+				}
+				j++
+			}
+			i++;
+		}
+		n++;
+	}
+	putchar ('\n');
 	return (0);
 }
