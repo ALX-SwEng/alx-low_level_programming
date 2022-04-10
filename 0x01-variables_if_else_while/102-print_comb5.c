de97de480ac8143bf8316a1447841c013c5c8b89
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-10 22:08:18 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* main - print combination of single digits
+* main - print combination of two digits
 * Return: program returns 0
 */
 int main(void)
@@ -10,15 +10,29 @@ int main(void)
 	while (n <= 9)
 	{
 		int i = n;
-		
+
 		while (i <= 9)
 		{
-			putchar ('0');
 			putchar (n % 10 + '0');
-			
 			putchar (i % 10 + '0');
-			putchar (',');
 			putchar (' ');
+			
+			int j = i;
+			
+			while (j <= 9)
+			{
+				int k = 0;
+				
+				while (k <= 9)
+				{
+					putchar (j % 10 + '0');
+					putchar (k % 10 + '0');
+					putchar (',');
+					putchar (' ');
+					k++;
+				}
+				j++;
+			}
 			i++;
 		}
 		n++;
