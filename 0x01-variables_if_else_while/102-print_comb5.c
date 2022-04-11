07b8ccf23b284c0d004a2cae75e3747bab7c2623
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-11 13:17:39 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -29,13 +29,11 @@ int main(void)
 						putchar(j + '0');
 						putchar(k + '0');
 
-					if (n + i + j + k < 227 && n < 57)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					else 
-					break;
+						if (n + i + j + k < 227 && n < 57)
+						{
+							putchar(',');
+							putchar(' ');
+						}
 					}
 					k++;
 				}
