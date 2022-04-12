ea89e4dfd8e0b2c83d02ff0cdb437e24d342c9da
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-12 16:38:56 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,16 +7,36 @@
  */
 void times_table(void)
 {
-    int i;
+int i;
     
-    for (i = 1; i <= 10; ++i)
-        _putchar ('0');
-    for (i = 1; i <= 9; ++i)
-    {
-        int j;
+for (i = 1; i <= 10; ++i)
+{
+_putchar ('0');
+_putchar (',');
+_putchar (',');
+}
+
+for (i = 1; i <= 9; ++i)
+{
+int j;
         
-        _putchar('0');        
-        for (j = i; j <= 9; ++j)
-            putchar(i * j);
-    }
+_putchar('0');        
+for (j = i; j <= 9; ++j)
+{
+int r, p = i * j;
+
+if (p > 9)
+{
+    r = p % 10;
+    p = p / 10;
+    _putchar(p);
+    _putchar(r);
+}
+else  
+_putchar(p);
+    
+_putchar (',');
+_putchar (',');
+}
+}
 }
