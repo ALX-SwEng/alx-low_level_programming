64a2932636918efd5a0ae7c709c2ee34848fb4a7
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-12 14:37:04 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,12 +1,11 @@
 #include "stdio.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_last_digit - extract the last digit of a number
+ * @n: an integer number
+ * Return: last digit of number.
  */
-int main(void)
+int print_last_digit(int n)
 {
-    print_alphabet();
-    return (0);
+return (n % 10);
 }
