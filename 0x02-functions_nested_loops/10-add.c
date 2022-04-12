c15c8cf180a4761c82417735302ef5b664cd8045
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-12 16:11:50 +0300

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,12 @@
 #include "stdio.h"
 
 /**
-* add - compute sum of two numbers 
+* add - compute sum of two numbers
 * @a: the 1st number to be added
 * @b: the 2nd number to be added
 * Return: sum of two numbers.
 */
 int add(int a, int b)
-{    
+{  
 return (a + b);
 }
