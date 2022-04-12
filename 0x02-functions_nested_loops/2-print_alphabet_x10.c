695412351ab2b36911b27d0a3057083ea4895d59
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-12 12:29:04 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,17 +6,18 @@
  * Return: none.
  */
 void print_alphabet_x10(void)
-{
-	char c = 'a';
+{	
 	int i;
-	
+
 	for (i = 1; i <= 10; ++i)
 	{
-	while (c <= 'z')
-	{
-		_putchar(c);
-		c++;
-	}
-	_putchar('\n');
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
 	}
 }
