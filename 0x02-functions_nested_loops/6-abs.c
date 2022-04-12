ec27b648c6e250ff52f849218ad1a14ae372e143
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-12 14:33:35 +0300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,13 +2,13 @@
 
 /**
  * _abs - compute the absolute value of integer
- * @n - an integer number
+ * @n: an integer number
  * Return: absolute value.
  */
 int _abs(int n)
 {
 if (n < 0)
-return (-1*n);
+return (-1 * n);
 else
-return (n);    
+return (n);
 }
