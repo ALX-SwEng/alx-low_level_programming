46415f55b41c474840b274d704b0ebc1ebd7e716
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-12 12:35:08 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,14 @@
 #include "stdio.h"
 
 /**
- * int _islower - check lowercase alphabets
+ * _islower - check lowercase alphabets
  *
  * Return: 1 if c is lowercase othrwise 0.
  */
 int _islower(int c)
 {
-    if (c >= 97 && c <= 122)
-        return (1);
-    else
-        return (0);
+        if (c >= 97 && c <= 122)
+            return (1);
+        else
+            return (0);
 }
