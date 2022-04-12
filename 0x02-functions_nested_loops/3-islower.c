ea168d979a6e941147578c9c55a8f7b37ee2c421
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-12 14:01:09 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,18 +2,14 @@
 
 /**
  * _islower - check lowercase alphabets
- *
+ * @c: letter input by user
  * Return: 1 if c is lowercase othrwise 0.
  */
 
 int _islower(int c)
 {
-        if (c >= 97 && c <= 122)
-        {
-                return (1);
-        }
-        else
-        {
-                return (0);
-        }
+if (c >= 97 && c <= 122)
+        return (1);
+else
+        return (0);
 }
