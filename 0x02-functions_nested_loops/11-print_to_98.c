2bf0d7af5683036c2000a20d663ea60106fb82e5
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-13 12:17:18 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,21 +8,45 @@
  */
 void print_to_98(int n)
 {
-    if (n < 98)
-    {
-        while (n <= 98)
-        {
-            _putchar (n);
-            ++n;
-        }
-    }
-    else
-    {
-        while (n >= 98)
-        {
-            _putchar (n);
-            --n;
-        }
-    }
-     _putchar ('\n');
+    while (n != 98)
+	{
+		int sign = 0;
+		int temp = n, reverse = 0, len = 0;;
+		
+	    if (n < 0)
+	    {
+	    	temp = -1 * n;
+	    	sign = 1;
+		}
+	    
+		if (temp == 0)
+			len = 1;
+		    
+	    while (temp > 0)
+	    {
+	        reverse = (reverse * 10) + (temp % 10);
+	        temp = temp / 10;
+	        len++;
+	    }
+	
+		if (sign == 1)
+			putchar('-');
+	
+	    while (len > 0)
+	    {
+	        putchar (reverse % 10 + '0');
+	        reverse = reverse / 10;
+	        len--;
+	    }
+	    
+        putchar (','); 
+		putchar (' ');
+	
+		if (n > 98)					
+			--n;  
+	    else
+	    	++n;
+	}
+	putchar (9 + '0'); 
+	putchar (8 + '0');
 }
