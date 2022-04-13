60dcb477438ccf907b36617501234acd1476bfce
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-13 09:27:20 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,28 +9,27 @@
 void jack_bauer(void)
 {
 	int n;
-    
-    for (n = 0; n < 3; ++n)
-    {
-            int i;
-    
-            for (i = 0; i < 4; ++i)
-            {
-                    int j;
-                
-                    for (j = 0; j < 6; ++j)
-                    {
-                            int k;
-                        
-                            for (k = 0; k <= 9; ++k)
-                            {
-                                _putchar ('0' + n);
+	for (n = 0; n < 3; ++n)
+	{
+		int i;
+		
+		for (i = 0; i < 4; ++i)
+		{
+			int j;
+			for (j = 0; j < 6; ++j)
+			{
+				int k;
+				
+				for (k = 0; k <= 9; ++k)
+				{
+				_putchar ('0' + n);
                                 _putchar ('0' + i);
+                                _putchar (':');
                                 _putchar ('0' + j);
                                 _putchar ('0' + k);
                                 _putchar ('\n');
-                            }
-                    }
-            }
-    }
+				}
+			}
+		}
+	}
 }
