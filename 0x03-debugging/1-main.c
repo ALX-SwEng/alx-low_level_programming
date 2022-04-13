7100b0c64168d9079690a64dcd3a7588eaa5ea6e
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-13 21:46:14 +0300

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,7 +12,7 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	/**
-	* i = 0;  
+	* i = 0;
 	* while (i < 10)
 	* {
 	* putchar(i);
@@ -20,6 +20,6 @@ int main(void)
 	*/
 
 	printf("Infinite loop avoided! \\o/\n");
-	
+
 	return (0);
 }
