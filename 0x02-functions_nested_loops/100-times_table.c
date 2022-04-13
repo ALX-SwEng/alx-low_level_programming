7d98a97fdc2fa51be749acf50a884d1f686fee51
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-13 18:29:43 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,7 @@
  */
 void print_times_table(int n)
 {
-	int i, a, j, r1, r2, r3, p = 0;
+	int i, a, j, p = 0;
 
 	if (n >= 0 && n <= 15)
 	{
@@ -15,39 +15,34 @@ void print_times_table(int n)
 		{
 			_putchar ('0');
 			_putchar (',');
-			for (a = 1; a <= 3; ++a)
-				_putchar (' ');	}}
+			for (a = 1; a <= 3; ++a) _putchar (' '); }				
 		_putchar ('0');
 		_putchar ('\n');
 		for (i = 1; i <= n; ++i)
 		{
 			for (j = 0; j <= n; ++j)
 			{
-				if (j == 0) 
-					_putchar ('0');
+				if (j == 0) _putchar ('0');
 				else
 				{
 					p = i * j;
+					int rev = 0, len = 0;
+					
+					while (p > 0)
+					{
+						rev = (rev * 10) + (p % 10);
+						++len;
+						p = p / 10; }
 					_putchar (',');
 					_putchar (' ');
-					if (p <= 9)
-					{ _putchar (' ');
-					 _putchar (' ');
-					 _putchar ('0' + p); }
-					else if (p <= 99)
-					{ _putchar (' ');
-					 r1 = p % 10;
-					 r2 = p / 10;		
-					 _putchar(r2 + '0');
-					_putchar(r1 + '0'); }
-					else
+					if (len < 3)
+					{ 
+						_putchar (' ');
+						if (len < 2) 
+							_putchar (' '); }
+					while (len > 0)
 					{
-						r1 = p % 10;
-						p = p / 10;
-						r2 = p % 10;
-						p = p / 10;
-						r3 = p % 10;
-						_putchar(r3 + '0');
-						_putchar(r2 + '0');
-						_putchar(r1 + '0'); }}}
-			_putchar ('\n');	}}}
+						_putchar ('0' + rev % 10);
+						rev = rev / 10;
+						--len; }}}
+			putchar ('\n');	}}}
