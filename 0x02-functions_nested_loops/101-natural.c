660f9327222020c5cd75d83bc0d6fde5c2d73d87
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-13 17:26:07 +0300

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,16 +7,17 @@
 
 int main(void)
 {
-	long int n = 0, sum = 0;
-	
+	int n = 0, sum = 0;
+
 	while (n < 1024)
 	{
 		if (n % 3 == 0 && n % 5 == 0)
 		{
 			sum += n;
 		}
+		++n;
 	}
 	printf("%ld", sum);
-	
+
 	return (0);
 }
