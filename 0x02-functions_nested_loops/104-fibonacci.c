5fac8ca22796e451dd62e9130eaa4cffb3b7fccf
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-13 19:28:05 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,16 +7,16 @@
 
 int main(void)
 {
-	long int n1 = 0, n2 = 1, n3, i;
+	unsigned long int n1 = 0, n2 = 1, n3, i;
 
 	for (i = 1; i <= 98; ++i)
 	{
 		n3 = n1 + n2;
 
-		if (i < 50)
-			printf("%ld, ", n3);
+		if (i < 98)
+			printf("%u, ", n3);
 		else
-			printf("%ld", n3);
+			printf("%u", n3);
 		n1 = n2;
 		n2 = n3;
 	}
