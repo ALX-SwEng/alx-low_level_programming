d631ac947b32a484276ca218834dfce8cb58bc66
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-13 08:46:40 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,11 @@
  */
 int print_last_digit(int n)
 {
-if (n == INT_MIN)
-n = 8;
-else if (n < 0)
-n = (-1 * n);
-	
-_putchar ( n % 10 + '0');
+	if (n == INT_MIN)
+		n = 8;
+	else if (n < 0)
+		n = (-1 * n);
+
+	_putchar ( n % 10 + '0');
 return (n);
 }
