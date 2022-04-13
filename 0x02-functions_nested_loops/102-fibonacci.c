c18633c75a6488611ded95f1febb4772f59c18e5
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-13 13:49:16 +0300

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,1 +1,27 @@
+**
+ * main - print fibnonic series
+ * 
+ * Return: Always success.
+ */
 
+int main()
+{    
+	 int n1 = 0, n2 = 1, n3, i;
+	     
+	 for(i = 2; i <= 48; ++i)
+	 {
+		  n3=n1+n2;
+		  if (n3 == 3)
+		  	putchar ('\n');
+		  
+		  if (i < 48 && n3 != 2)	
+		  printf("%d, ",n3);
+		  else 
+		  printf("%d",n3);
+		  
+		  n1 = n2;
+		  n2 = n3;
+	 }
+	 
+  return (0);  
+ } 
