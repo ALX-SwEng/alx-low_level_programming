e48d09d544ca0dd38c82e7fc7b5ff44740839edf
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-13 09:19:42 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,35 @@
-#include "stdio.h"
+#include "main.h"
 
 /**
- * main - check the code
+ * main - prints every minute of the day of Jack Bauer, 
+ * starting from 00:00 to 23:59.
  *
- * Return: Always 0.
+ * Return: none.
  */
-int main(void)
+void jack_bauer(void)
 {
-    print_alphabet();
-    return (0);
+    int n;
+    
+    for (n = 0; n < 3; ++n)
+    {
+            int i;
+    
+            for (i = 0; i < 4; ++i)
+            {
+                    int j;
+                
+                    for (j = 0; j < 6; ++j)
+                    {
+                            int k;
+                        
+                            for (k = 0; k <= 9; ++k)
+                            {
+                                    _putchar ('0' + n);
+                                    _putchar ('0' + i);
+                                    _putchar ('0' + j);
+                                    _putchar ('0' + k);
+                            }
+                    }
+            }
+    }
 }
