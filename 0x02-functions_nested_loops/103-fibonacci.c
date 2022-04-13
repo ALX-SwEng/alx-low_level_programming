e885d419bd242a159320b67cb03a78e9cf4fe236
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-13 16:43:13 +0300

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,15 +8,15 @@
 int main(void)
 {
 	long int n1 = 0, n2 = 1, n3, sum = 0;
-
-do{
+	
+	do{
 		n3 = n1 + n2;
 		if (n3 % 2 == 0)
-          sum += n3;
+			sum += n3;
 		n1 = n2;
 		n2 = n3;
 	}while (n3 <= 4000000);
-  
+
 	printf ("%ld\n", sum);
 
 	return (0);
