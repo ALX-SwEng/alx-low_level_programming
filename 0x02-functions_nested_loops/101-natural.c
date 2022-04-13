576e9518864d596b150735ab9943052681ea23f5
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-13 17:10:47 +0300

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+/**
+ * main - find sum of multiples of 3 and 5
+ *
+ * Return: Always success.
+ */
 
+int main(void)
+{
+  long int n = 0, sum = 0;
+  
+  while (n < 1024)
+  {
+    if (n % 3 == 0 && n % 5 == 0)
+    {
+      sum += n;
+    }
+  }
+  
+  printf("%ld", sum);
+
+	return (0);
+}
