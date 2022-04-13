fe8f183cc1292feef7afc428dda6efd2448f4bb3
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-13 12:33:38 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - prints all natural numbers from n to 98
+ * print_to_98 - prints all natural numbers from n to 98
  *@n: integer number
  *
  * Return: nothing.
@@ -15,35 +15,35 @@ void print_to_98(int n)
 
 		if (n < 0)
 		{
-	    		temp = -1 * n;
-	    		sign = 1;
+			temp = -1 * n;
+			sign = 1;
 		}
 		if (temp == 0)
 			len = 1;
 
 		while (temp > 0)
 		{
-	        	reverse = (reverse * 10) + (temp % 10);
-	        	temp = temp / 10;
-	        	len++;
+			reverse = (reverse * 10) + (temp % 10);
+			temp = temp / 10;
+			len++;
 		}
 
 		if (sign == 1)
 			_putchar('-');
 		while (len > 0)
 		{
-	        	_putchar(reverse % 10 + '0');
-	        	reverse = reverse / 10;
-	        	len--;
+			_putchar(reverse % 10 + '0');
+			reverse = reverse / 10;
+			len--;
 		}
 		_putchar (',');
 		_putchar (' ');
 		if (n > 98)				
 			--n;
 		else
-	    		++n;
+			++n;
 	}
-	_putchar (9 + '0'); 
+	_putchar (9 + '0');
 	_putchar (8 + '0');
 	_putchar ('\n');
 }
