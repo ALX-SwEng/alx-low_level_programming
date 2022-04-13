78ab02827c456639512f18b65d65f6583f025384
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-13 17:45:02 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,10 +7,10 @@
  */
 void print_times_table(int n)
 {
+	int i, a, j, r1, r2, r3, p = 0;
+	
 	if (n >= 0 && n <= 15)
 	{
-		int i, a;
-
 		for (i = 1; i <= n + 1; ++i)
 		{
 			_putchar ('0');
@@ -22,26 +22,31 @@ void print_times_table(int n)
 		_putchar ('\n');
 		for (i = 1; i <= n; ++i)
 		{
-			int j, r1, r2, r3, p = 0;
-
 			for (j = 0; j <= n; ++j)
 			{
 				if (j == 0) _putchar ('0');
 				else
 				{
 					p = i * j;
-					_putchar (','); _putchar (' ');
+					_putchar (','); 
+					_putchar (' ');
 					if (p <= 9)
-					{ _putchar (' '); _putchar (' '); _putchar ('0' + p); }
+					{ _putchar (' '); 
+					 _putchar (' '); 
+					 _putchar ('0' + p); }
 					else if (p <= 99)
-					{
-						_putchar (' ');
-						r1 = p % 10; r2 = p / 10;				
-						_putchar(r2 + '0');
+					{ _putchar (' ');
+					 r1 = p % 10; 
+					 r2 = p / 10;			
+					 _putchar(r2 + '0');
 						_putchar(r1 + '0'); }
 					else
-					{		
-						r1 = p % 10; p = p / 10; r2 = p % 10; p = p / 10; r3 = p % 10;
+					{
+						r1 = p % 10; 
+						p = p / 10; 
+						r2 = p % 10; 
+						p = p / 10; 
+						r3 = p % 10;
 						_putchar(r3 + '0');
 						_putchar(r2 + '0');
 						_putchar(r1 + '0'); }}}
