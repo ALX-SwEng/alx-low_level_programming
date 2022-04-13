5086433ef2056e06debc1005adfc1d70a6e7dbf4
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-13 21:49:14 +0300

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,25 +10,24 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
-
-    if (a > b) 
-    {
-      if (b > c)
-      {
-        largest = a;
-      }
-      else if (a > c)
-      {
-        largest = a;
-      }
-      else
-      {
-        largest = c;
-      }
-    }
-    else if (b > c)
-    {
+	int largest;
+	if (a > b) 
+	{
+		if (b > c)
+		{
+			largest = a;
+		}
+		else if (a > c)
+		{
+			largest = a;
+		}
+		else
+		{
+			largest = c;
+		}
+	}
+	else if (b > c)
+	{
         largest = b;
     }
     else
