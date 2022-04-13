b10a1f74c86a0aaea7dccf82e07b212e89a8794d
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-13 08:58:02 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <limits.h>
-#include <math.h>
+#include <stdlib.h>
 
 /**
  * print_last_digit - extract the last digit of a number
@@ -12,7 +12,7 @@ int print_last_digit(int n)
 	if (n == INT_MIN)
 		n = 8;
 	if (n < 0)
-		n = fabs(n);
+		n = abs(n);
 
 	_putchar (n % 10 + '0');
 
