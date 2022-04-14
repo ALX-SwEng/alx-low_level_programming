bf81c1b2cf6c86619c537b136fe3da8fb63a812c
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-14 05:37:18 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,16 +9,19 @@
 
 int main(void)
 {
-	unsigned long int n1 = 0, n2 = 1, n3, i;
+	unsigned long int n1 = 0, n2 = 1, n3, i, j = 1;
 
 	for (i = 1; i <= 98; ++i)
 	{
 		n3 = n1 + n2;
 
-		if (i < 98)
+		if (i < 93)
 			printf("%lu, ", n3);
-		else
-			printf("%lu", n3);
+		else if (i < 93)
+		{
+			printf("%lu", n3 + (j * 18446744073709,551616));
+			++j;
+		}
 		n1 = n2;
 		n2 = n3;
 	}
