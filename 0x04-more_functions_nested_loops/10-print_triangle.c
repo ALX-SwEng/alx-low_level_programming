2ce2b7977bce3f127a80c31e8fddfa9256f7d57f
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-14 21:22:42 +0300

    Create 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_alphabet -  Make a triangle with #
+ * @size: size of the triangle
+ * Return: none.
+ */
+void print_triangle(int size)
+{
+	if (size > 0)
+	{
+		int i;
+
+		for (i = 0; i < size; ++i)
+		{
+			int j;
+			
+			for (j = size; j > i; --j)    
+				_putchar(' ');
+			
+			int k;
+			
+			for (k = 0; k <= i; ++k)    
+				_putchar('#');
+			
+			_putchar('\n');	
+		}	
+	}
+	else
+		_putchar('\n');
+}
