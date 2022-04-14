aaf3a67ebc787873596a3006561d0d214f977c7e
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-14 21:44:07 +0300

    Create 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet -  Make a triangle with #
+ * @c: data
+ * Return: 1 if c is digit otherwise 0.
+ */
+int _isdigit(int c)
+{
+    if (if c >= 0 && c <= 9)
+    {
+        return (1);
+    }
+	else
+	{
+		return (0);
+    }
+}
