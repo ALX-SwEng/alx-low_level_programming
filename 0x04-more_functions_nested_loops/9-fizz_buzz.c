8a7da0d6cdd6d943cd61f3f46b41e63ffd68d22c
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-15 07:35:29 +0300

    Create 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "stdio.h"
+
+/**
+* main - prints the numbers from 1 to 100
+* 
+* Return: Always success.
+*/
+
+int main (void)
+{
+	int i;
+
+	for (i = 1; i <= 100; ++i)
+	{
+		if (i > 1)
+			putchar(' ');
+
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+
+		else if (i % 3 == 0 )
+			printf("Fizz");
+
+		else if (i % 5 == 0)
+        	printf("Buzz");
+
+        else
+        	printf("%d", i);
+	}
+}
