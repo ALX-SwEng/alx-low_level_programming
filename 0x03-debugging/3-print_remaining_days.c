cdcedde0fb9ecf12e29ebce0528ec9fb4115bcf3
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-15 08:00:12 +0300

    Update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -22,7 +22,7 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month == 2 && day == 60 && year % 4 != 0)
+		if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 30, year);
 		}
