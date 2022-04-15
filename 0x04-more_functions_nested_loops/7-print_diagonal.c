d530246e64130ca9560f3768197e3f0715def031
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-15 06:49:49 +0300

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,11 +14,11 @@ void print_diagonal(int n)
 
 		for (i = 1; i <= n; ++i)
 		{
+			_putchar('\n');
 			for (j = 1; j < i; ++j)
 				_putchar(' ');
 
 			_putchar('\\');
-			_putchar('\n');
 		}
 	}
 	else
