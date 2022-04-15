e5c43ba4cb9002c2b541710c264da4781f87474d
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-15 05:27:08 +0300

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,10 +7,10 @@
  */
 int _isdigit(int c)
 {
-    if (if c >= 0 && c <= 9)
-    {
-        return (1);
-    }
+    	if (if c >= 48 && c <= 57)
+    	{
+        	return (1);
+    	}
 	else
 	{
 		return (0);
