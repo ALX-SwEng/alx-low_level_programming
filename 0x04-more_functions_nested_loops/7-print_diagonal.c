cf9a0f5b6963095d8e5ec0e5b338e6cbbc0645d0
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-15 06:45:23 +0300

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,11 +11,12 @@ void print_diagonal(int n)
 	if (n > 0)
 	{
 		int i, j;
+
 		for (i = 1; i <= n; ++i)
 		{
 			for (j = 1; j < i; ++j)
 				_putchar(' ');
-				
+
 			_putchar('\\');
 			_putchar('\n');
 		}
