6aec0b332a6dae6f2ec572d192008ba87d75b559
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-15 21:31:50 +0300

    Create 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - a function that prints an integer.
+ * @n: number
+ * Return: Always 0.
+ */
+
+void print_number(int n)
+{
+  int rev = 0;
+  
+  while (n > 0)
+  {
+    rev = (rev * 10) + (t % 10)
+    n = n / 10;
+  }
+  
+  while (t > 0)
+  {
+    _putchar ('0' + rev % 10);
+    rev = rev / 10;
+  }
+}
