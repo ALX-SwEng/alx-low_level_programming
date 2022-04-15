33665fef46c3d8153a8f8f88112fa2e5c38082a5
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-15 20:26:45 +0300

    Update 3-main_a.c

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -13,15 +13,26 @@ int main(void)
     int day;
     int year;
 
-    month = 4;
+   month = 4;
     day = 01;
-    year = 1997;
+    year = 1997; 
 
     printf("Date: %02d/%02d/%04d\n", month, day, year);
-
     day = convert_day(month, day);
-
     print_remaining_days(month, day, year);
-
+        
+    month = 2;
+    day = 29;
+    year = 2000;
+    printf("Date: %02d/%02d/%04d\n", month, day, year);
+    day = convert_day(month, day);
+    print_remaining_days(month, day, year);
+  
+    month = 12;
+    day = 31;
+    year = 4000;
+    printf("Date: %02d/%02d/%04d\n", month, day, year);
+    day = convert_day(month, day);
+    print_remaining_days(month, day, year);
     return (0);
 }
