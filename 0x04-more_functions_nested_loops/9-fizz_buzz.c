8fe7557da2f535e93c6f2a35c44f807a527aa97c
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-15 07:40:15 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,11 +2,11 @@
 
 /**
 * main - prints the numbers from 1 to 100
-* 
+*
 * Return: Always success.
 */
 
-int main (void)
+int main(void)
 {
 	int i;
 
@@ -16,15 +16,15 @@ int main (void)
 			putchar(' ');
 
 		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
+		printf("FizzBuzz");
 
-		else if (i % 3 == 0 )
-			printf("Fizz");
+		else if (i % 3 == 0)
+		printf("Fizz");
 
 		else if (i % 5 == 0)
         	printf("Buzz");
 
-        else
+        	else
         	printf("%d", i);
 	}
 return (0);
