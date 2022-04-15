60e1ac40156977225d92bbeaa322e49de3e3ab7f
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-15 22:17:14 +0300

    Update main.c

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <limits.h>
 
 /**
  * main - check the code.
@@ -18,5 +19,24 @@ int main(void)
     _putchar('\n');
     print_number(-98);
     _putchar('\n');
+    
+    print_number(3);
+    _putchar('\n');
+    print_number(-1);
+    _putchar('\n');
+    print_number(10);
+    _putchar('\n');
+    print_number(-99);
+    _putchar('\n');
+    
+    print_number(-4096);
+    _putchar('\n');
+    print_number(123456789);
+    _putchar('\n');
+    print_number(INT_MIN);
+    _putchar('\n');
+    print_number(INT_MAX);
+    _putchar('\n');
+    
     return (0);
 }
