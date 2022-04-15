458d597f3d6708a9eccd5f50e6afe3463137a42e
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-15 20:02:27 +0300

    Update 3-convert_day.c

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -1,3 +1,13 @@
+include "main.h"
+
+/**
+* convert_day - converts day of month to day of year, without accounting
+* for leap year
+* @month: month in number format
+* @day: day of month
+* Return: day of year
+*/
+
 int convert_day(int month, int day)
 {
     switch (month)
