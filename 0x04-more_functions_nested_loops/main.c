df7a6d2c864c8e37bcb909824885187dfb6bbcb8
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-15 22:39:24 +0300

    Update main.c

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -8,16 +8,7 @@
  * Return: Always 0.
  */
 int main(void)
-{
-    print_number(-98);
-    _putchar('\n');
-    
-    print_number(-4096);
-    _putchar('\n');
-    
-    print_number(123456789);
-    _putchar('\n');
-
+{ 
     printf("%d", INT_MIN);
     _putchar('\n');
 
