d1bd8611fe3e7de84774180308f3ca642c81663b
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-15 23:37:33 +0300

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,13 +5,20 @@
  * @n: number
  * Return: Always 0.
  */
-
 void print_number(int n)
 {
-	int rev = 0, len = 0, sign = 0;
+	unsigned long long int rev = 0;
+	int temp, len = 0, sign = 0;
 
 	if (n == 0)
 		len = 1;
+
+	else if (n == INT_MIN)
+	{
+		n = 2147483647;
+		temp = 1;
+		sign = 1;
+	}
 	else if (n < 0)
 	{
 		n = -1 * n;
@@ -20,17 +27,21 @@ void print_number(int n)
 
 	while (n > 0)
 	{
-		rev = (rev * 10) + (n % 10);
+		int rem = n % 10;
+		if (temp == 1)
+		{
+			rem += 1;
+			temp = 0;
+		}
+		rev = (rev * 10) + rem;
 		n = n / 10;
 		++len;
 	}
-
 	if (sign == 1)
-		_putchar ('-');
-
+		putchar ('-');
 	while (len > 0)
 	{
-		_putchar ('0' + rev % 10);
+		putchar ('0' + rev % 10);
 		rev = rev / 10;
 		--len;
 	}
