ed34e1a24c453988671bfad5f5757ff9a62ee4a7
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-15 06:29:04 +0300

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line -  Make a line with _
+ * @n: length of the line
+ * Return: none.
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int i;
+
+		for (i = 1; i <= n; ++i)
+				_putchar('#');
+	}
+		_putchar('\n');
+}
