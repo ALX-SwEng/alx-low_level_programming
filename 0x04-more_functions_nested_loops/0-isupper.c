7ec78694fc64a30ff3ba8e25b534595209a8a26a
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-15 05:35:13 +0300

    Create 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper -  Make a triangle with #
+ * @c: character
+ * Return: 1 if c is uppercase 0.
+ */
+int _isupper(int c)
+{
+    if (if c >= 'A' && c <= 'Z')
+		return (1);
+    else
+		return (0);
+}
