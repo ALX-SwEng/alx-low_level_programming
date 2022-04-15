2df847b2a6a58f0523d016f425d11301d7652b56
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-15 07:41:20 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,16 +16,16 @@ int main(void)
 			putchar(' ');
 
 		if (i % 3 == 0 && i % 5 == 0)
-		printf("FizzBuzz");
+			printf("FizzBuzz");
 
 		else if (i % 3 == 0)
-		printf("Fizz");
+			printf("Fizz");
 
 		else if (i % 5 == 0)
-        	printf("Buzz");
+			printf("Buzz");
 
         	else
-        	printf("%d", i);
+			printf("%d", i);
 	}
 return (0);
 }
