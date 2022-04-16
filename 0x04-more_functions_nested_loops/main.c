d94624aabdb91dd9409268952fe7c3e3ad649251
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-16 05:30:35 +0300

    Update main.c

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -9,7 +9,15 @@
  */
 int main(void)
 { 
-     print_number(INT_MAX);
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-98);
     _putchar('\n');
     
     return (0);
