6464598fe7c74b8adfab183c1f7a7418181e2392
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-18 16:09:32 +0300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,12 +11,13 @@
 char *_strcpy(char *dest, char *src)
 {
 	int len = 0;
+
 	while (*(src + len) != '\0')
 	{
 		*(dest + len) = *(src + len);
 		++len;
 	}
-	
+
 	*(dest + len) = '\0';
 
 	return (src);
