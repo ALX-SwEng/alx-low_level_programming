ff50f057ea9037dc6c546b5eebbf053322f656f9
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-18 09:43:38 +0300

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+/**
+ * print_rev -   prints a string in reverse, followed by a new line, to stdout.
+ * @s: string
+ *
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+	{
+		++len;
+	}
+	
+	--len;
+	while (len >= 0)
+	{
+		putchar(*(s + len));
+		--len;
+	}
+}
