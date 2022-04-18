ee7d48f7a72129adc7fc4f1cb6a4347bc451b1a7
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-18 22:38:48 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,17 +7,21 @@ int main()
 {
     srand((unsigned int)(time(NULL)));
     int i;
+ 
     char pass[12];
 
-    printf("Press enter to get a twelve-character password\n");
-    getchar();
-
-    for (i = 0; i < 4; i++) {
+    for (i = 0; i < 4; i++) 
+    {
         pass[i] = rand() % 9;
-        char capLetter = 'A' + (rand() % 26);
+        char capLetter;
+        
+        capLetter = 'A' + (rand() % 26);
         pass[i + 2] = capLetter;
-        char letter = 'a' + (rand() % 26);
+        char letter;
+
+        letter = 'a' + (rand() % 26);
         pass[i + 3] = letter;
+
         printf("%d%c%c", pass[i], pass[i + 2], pass[i + 3]);
     }
     printf("\n\n");
