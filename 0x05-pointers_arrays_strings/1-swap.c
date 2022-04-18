267dbbb5b936fd72c9566e4f914fdf891bb2c17d
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-18 09:17:13 +0300

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * reset_to_98 - change value using pointer
- * @n: integer value
+ * reset_to_98 -  swaps the values of two integers. 
+ * @a: 1st integer number
+ * @b: 2nd integer number
  *
  * Return: none
  */
 void swap_int(int *a, int *b)
 {
 	*a = *a + *b;
-  *b = *a - *b;
-  *a = *a - *b;
+	*b = *a - *b;
+	*a = *a - *b;
 }
