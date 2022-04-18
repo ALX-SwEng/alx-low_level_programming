282fa300cc919e78c7f8e33688c766868529b298
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-18 09:26:27 +0300

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,7 +4,6 @@
  *
  * Return: string len
  */
- 
 int _strlen(char *s)
 {
 	int len = 0;
@@ -12,5 +11,5 @@ int _strlen(char *s)
 	while (*(s + len) != '\0')
 		++len;
 
-	return len;
+	return (len);
 }
