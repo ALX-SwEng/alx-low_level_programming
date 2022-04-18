9695ef4e092459b717bd8c7a98bb4e54f74bc6f4
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-18 18:07:23 +0300

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,34 +14,33 @@ int _atoi(char *s)
 	{
 	    if (*(s + len) >= '0' && *(s + len) <= '9')
 	    {
-		    found = 1;
-			if (*(s + len) == '0')
-				num = num * 10;
-			else if (*(s + len) == '1')
-			    num = (num * 10) + 1;
-		    else if (*(s + len) == '2')
-			    num = (num * 10) + 2;
-		    else if (*(s + len) == '3')
-				num = (num * 10) + 3;
-			else if (*(s + len) == '4')
-				num = (num * 10) + 4;
-			else if (*(s + len) == '5')
-				num = (num * 10) + 5;
-			else if (*(s + len) == '6')
-				num = (num * 10) + 6;
-			else if (*(s + len) == '7')
-			   	num = (num * 10) + 7;
-			else if (*(s + len) == '8')
-			   	num = (num * 10) + 8;
-			else if (*(s + len) == '9')
-			  	num = (num * 10) + 9;
-	
-			c = *(s + len - 1);
-			if (c == '-' || c == '+')
-				sign = c;
-			c = *(s + len + 1);
-			if (!(c >= '0' &&  c <= '9'))
-				break;
+		found = 1;
+		if (*(s + len) == '0')
+			num = num * 10;
+		else if (*(s + len) == '1')
+		    num = (num * 10) + 1;
+		else if (*(s + len) == '2')
+		    num = (num * 10) + 2;
+		else if (*(s + len) == '3')
+			num = (num * 10) + 3;
+		else if (*(s + len) == '4')
+			num = (num * 10) + 4;
+		else if (*(s + len) == '5')
+			num = (num * 10) + 5;
+		else if (*(s + len) == '6')
+			num = (num * 10) + 6;
+		else if (*(s + len) == '7')
+		   	num = (num * 10) + 7;
+		else if (*(s + len) == '8')
+		   	num = (num * 10) + 8;
+		else if (*(s + len) == '9')
+		  	num = (num * 10) + 9;
+		c = *(s + len - 1);
+		if (c == '-' || c == '+')
+			sign = c;
+		c = *(s + len + 1);
+		if (!(c >= '0' &&  c <= '9'))
+			break;
 		}
 		++len; }
 	if (found == 1 && sign == '-')
