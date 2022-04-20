46718f54b00d884ed0915e003baf1d28a3667c2a
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-20 20:41:27 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * string_toupper - changes all lowercase letters of a string to uppercase.  
+ * string_toupper - changes all lowercase letters of a string to uppercase.
  * @str: string pointer
  *
  * Return: string in uppercase.
