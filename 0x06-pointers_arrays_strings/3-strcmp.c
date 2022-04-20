ef1e44ab5a7e6b0ab4c29ead8729173ae64f6a4f
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-20 21:22:25 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,29 @@
-#include "main.h"
-
 /**
- * _strcmp -  
+ * _strcmp - compares two strings.
+ * @s1: first string
+ * @s2: second string
  *
  * Return: string pointer.
  */
 
 int _strcmp(char *s1, char *s2)
+{
+	int i = 0, flag = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] < s2[i])
+    {
+      flag = -15;
+      break;
+    }
+    else if (s1[i] > s2[i])
+    {
+			flag = 15;
+      break;
+    }
+		i++;
+	}
+  
+  return (flag);
+}
