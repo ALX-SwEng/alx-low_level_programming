c0ef1391b96815bda37d6e0c1f96236e27d1e1d7
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-20 21:05:40 +0300

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,14 +9,13 @@
 void reverse_array(int *a, int n)
 {
 	int i = 0, temp;
-	
+
 	--n;
-	
 	while (n > i)
 	{
-		temp = *(a + i);
-		*(a + i) = *(a + n);
-		*(a + n) = temp;
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
 		++i;
 		--n;
 	}
