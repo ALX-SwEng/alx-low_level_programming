43e7b2603ca7cbabeaa7799ad1b98fd7a8c0e199
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-20 20:30:19 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,9 +1,23 @@
 #include "main.h"
 
 /**
- * reverse_array -  
+ * string_toupper - changes all lowercase letters of a string to uppercase.  
+ * @str: string pointer
  *
- * Return: string pointer.
+ * Return: string in uppercase.
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		if (*(str + i) >= 'a' && *(str + i) <= 'z')
+		{
+			*(str + i) = *(str + i) - 32;
+		}
+		i++;
+	}
+	return (str);
+}
