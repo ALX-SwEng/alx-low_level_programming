72ed503a20806939afd3ab8c799d5235064ee5aa
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-20 20:39:08 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -14,9 +14,7 @@ char *string_toupper(char *str)
 	while (*(str + i) != '\0')
 	{
 		if (*(str + i) >= 'a' && *(str + i) <= 'z')
-		{
 			*(str + i) = *(str + i) - 32;
-		}
 		i++;
 	}
 	return (str);
