9c16c7e64c1bf338f13577977f337d9c9de8fa8f
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-22 00:47:47 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,19 +9,15 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int len1 = 0, len2 = 0, i;
+	int len1 = 0, len2;
 
 	while (*(dest + len1) != '\0')
 		len1++;
 
-	while (*(src + len2) != '\0')
-	{
-		i = len1 + len2;
-		*(dest + i) = *(src + len2);
-		++len2;
-	}
+	for (len2 = 0; src[len2] != '\0'; ++len2)
+		dest [len1 + len2] = src[len2];
 
-	*(dest + i + 1) = '\0';
+	dest[len1 + len2] = '\0';
 
 	return (dest);
 }
