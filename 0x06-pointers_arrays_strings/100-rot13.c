025f97862ab3c6e512e919017b48c6db6830a9e4
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-22 02:58:49 +0300

    Create 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string using rot13.
+ * @str: string pointer
+ *
+ * Return: encoded string.
+ */
+ 
+char *rot13(char *str)
+{
+	int i = 0;
+	unsigned char s;
+
+	while (*(str + i) != '\0')
+	{
+		s = *(str + i);
+		if (s >= 'a' && s <= 'z')
+		{
+			s += 13;
+			if (s > 'z')
+				s -= 26;
+			str[i] = s;
+		}
+		else if (s >= 'A' && s <= 'Z')
+		{	
+			s += 13;
+			if (s > 'Z')
+				s -= 26;
+			str[i] = s;
+		}
+		i++;
+	}
+	return (str);
+}
