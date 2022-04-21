ace6955b68ed40732c061d43c083fc14133519db
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-21 20:33:03 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,16 +13,16 @@ char *leet(char *str)
 	{
 		s = *(str + i);
 
-		if (s >= 'a' || s <= 'A')
-      *(str + i) = '4';
-		else if (s >= 'e' || s <= 'E')
-      *(str + i) = '3';
-		else if (s >= 'o' || s <= 'O')
-      *(str + i) = '0';
-		else if (s >= 't' || s <= 'T')
-      *(str + i) = '7';
-    else if (s >= 'l' || s <= 'L')
-      *(str + i) = '1';
+		if (s == 'a' || s == 'A')
+			*(str + i) = '4';
+		else if (s == 'e' || s == 'E')
+			*(str + i) = '3';
+		else if (s == 'o' || s == 'O')
+			*(str + i) = '0';
+		else if (s == 't' || s == 'T')
+			*(str + i) = '7';
+    	else if (s == 'l' || s == 'L')
+			*(str + i) = '1';
 
 		i++;
 	}
