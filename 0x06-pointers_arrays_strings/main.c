d9fa6453199fcafa2b9cc1c4196a4e0b9b79ca23
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-22 01:35:03 +0300

    Update main.c

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -8,12 +8,12 @@
  */
 int main(void)
 {
-     char s1[] = "Hello";
-    char s2[] = "World!";
+    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+    char *ptr;
 
-    printf("%d\n", _strcmp(s1, s2));
-    printf("%d\n", _strcmp(s2, s1));
-    printf("%d\n", _strcmp(s1, s1));
+    ptr = cap_string(str);
+    printf("%s", ptr);
+    printf("%s", str);
  
     return (0);
 }
