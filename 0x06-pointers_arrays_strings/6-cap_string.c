15ebfc0fd543506c1833b861e74867fc020c5e5f
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-21 19:26:02 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,9 +1,24 @@
-#include "main.h"
-
 /**
- * reverse_array -  
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ * @str: string pointer
  *
- * Return: string pointer.
+ * Return: string in uppercase.
  */
+char *cap_string(char *str)
+{
+	int i = 0;
+  char s1, s2;
 
-char *cap_string(char *)
+	while (*(str + i) != '\0')
+	{
+    s1 = *(str + i);
+    s2 = *(str + i - 1);
+
+		if (s1 >= 'a' && s1 <= 'z')
+      if (s2 == '\n' || s2 == ',' || s2 == ';' || s2 == '.' || s2 == '!' || s2 == '\?' ||
+         s2 == '\"' || s2 == '(' || s2 == ')' || s2 == '{' || s2 == '}')
+        *(str + i) = s1 - 32;
+		i++;
+	}
+	return (str);
+}
