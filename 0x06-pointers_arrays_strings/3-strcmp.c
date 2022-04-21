b2f7cd648c276fa805a5ee50d1c87d4589e5e70f
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-22 01:03:52 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,16 +15,8 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] < s2[i])
-		{
-			flag = -1;
-			break;
-		}
-		else if (s1[i] > s2[i])
-		{
-			flag = 1;
-			break;
-		}
+			return (s1[i] - s2[i]);
 		i++;
 	}
-return (flag);
+return (0);
 }
