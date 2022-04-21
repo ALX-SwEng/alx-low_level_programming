d8bf3f63bfeb97dce6ac1f0c4eff4b3d12363238
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-21 20:22:06 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,9 +1,31 @@
-#include "main.h"
-
 /**
- * reverse_array -  
+ * leet - encodes a string into 1337.
+ * @str: string pointer
  *
- * Return: string pointer.
+ * Return: encoded string.
  */
+char *leet(char *str)
+{
+	int i = 0;
+	char s;
 
-char *leet(char *);
+	while (*(str + i) != '\0')
+	{
+		s = *(str + i);
+
+		if (s >= 'a' || s <= 'A')
+      *(str + i) = '4';
+		else if (s >= 'e' || s <= 'E')
+      *(str + i) = '3';
+		else if (s >= 'o' || s <= 'O')
+      *(str + i) = '0';
+		else if (s >= 't' || s <= 'T')
+      *(str + i) = '7';
+    else if (s >= 'l' || s <= 'L')
+      *(str + i) = '1';
+
+		i++;
+	}
+
+	return (str);
+}
