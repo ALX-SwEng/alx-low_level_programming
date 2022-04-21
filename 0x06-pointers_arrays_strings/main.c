0b65559fa0519f9eebe5258a4b568e908e1ec133
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-21 22:01:24 +0300

    Update main.c

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -8,12 +8,16 @@
  */
 int main(void)
 {
-    char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
-    char *p;
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
 
-    p = leet(s);
-    printf("%s", p);
-    printf("%s", s);
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strcat(s1, s2);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
  
     return (0);
 }
