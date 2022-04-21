c8dcce398e4adfb68893ff0b083a637905adcb82
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-22 01:07:28 +0300

    Update main.c

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -8,21 +8,12 @@
  */
 int main(void)
 {
-        char s1[98] = "Hello ";
-    char s2[] = "World!\n";
-    char *ptr;
+     char s1[] = "Hello";
+    char s2[] = "World!";
 
-    printf("%s\n", s1);
-    printf("%s", s2);
-    ptr = _strncat(s1, s2, 1);
-    printf("%s\n", s1);
-    printf("%s", s2);
-    printf("%s\n", ptr);
-    ptr = _strncat(s1, s2, 1024);
-    printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", ptr);
-    
+    printf("%d\n", _strcmp(s1, s2));
+    printf("%d\n", _strcmp(s2, s1));
+    printf("%d\n", _strcmp(s1, s1));
  
     return (0);
 }
