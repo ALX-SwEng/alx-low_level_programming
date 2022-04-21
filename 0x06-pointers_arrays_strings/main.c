a23423981ee15c9a090470b402f5928bd131e583
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-21 23:13:12 +0300

    Update main.c

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -8,20 +8,35 @@
  */
 int main(void)
 {
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
+    char s1[98];
     char *ptr;
+    int i;
 
+    for (i = 0; i < 98 - 1; i++)
+    {
+        s1[i] = '*';
+    }
+    s1[i] = '\0';
     printf("%s\n", s1);
-    printf("%s", s2);
-    ptr = _strncat(s1, s2, 1);
+    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
     printf("%s\n", s1);
-    printf("%s", s2);
     printf("%s\n", ptr);
-    ptr = _strncat(s1, s2, 1024);
+    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
     printf("%s", s1);
-    printf("%s", s2);
     printf("%s", ptr);
+    for (i = 0; i < 98; i++)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", s1[i]);
+    }
+    printf("\n");
  
     return (0);
 }
