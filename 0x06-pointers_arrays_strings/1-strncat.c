615c3899c0efc71992ca76bb6e3566b81728277c
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-22 00:52:26 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,10 +15,10 @@ char *_strncat(char *dest, char *src, int n)
 
 	while (*(dest + len1) != '\0')
 		len1++;
-	
+
 	for (len2 = 0 ; len2 < n && src[len2] != '\0' ; len2++)
 		dest[len1 + len2] = src[len2];
-	
+
 	dest[len1 + len2] = '\0';
 
 	return (dest);
