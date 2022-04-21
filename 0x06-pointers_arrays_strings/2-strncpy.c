68883bc80d684f10367b8c5e7fb3fcad851f17f4
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-21 23:10:35 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,9 +1,23 @@
 #include "main.h"
 
 /**
- * _strncpy -  
+ * _strncpy - concatenates two strings.
+ * @dest: destination string
+ * @src: source string
+ * @n: no. of character to be concatnated.
  *
- * Return: string pointer.
+ * Return: concatnated string
  */
 
 char *_strncpy(char *dest, char *src, int n)
+{
+	int len = 0;
+
+	while (*(src + len) != '\0' && len2 < n)
+	{
+		*(dest + len) = *(src + len);
+		++len;
+	}
+
+	return (dest);
+}
