583f40a66cba0715919784175293746881dd6d48
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-22 21:56:45 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,11 +14,11 @@ int main(void)
 	unsigned long int n1 = 0;
 	unsigned long int n2 = 1;
 	unsigned long int n3;
-	unsigned long int l = 1000000000;
-	unsigned long int n2a;
-	unsigned long int n2b;
-	unsigned long int n3a;
-	unsigned long int n3b;
+	unsigned long int limit = 1000000000;
+	unsigned long int a1;
+	unsigned long int a2;
+	unsigned long int b1;
+	unsigned long int b2;
 
 	for (i = 1; i < 91; i++)
 	{
@@ -29,20 +29,20 @@ int main(void)
 		n2 = n3;
 	}
 
-	n2a = (n2 / l);
-	n2b = (n2 % l);
-	n3a = (n3 / l);
-	n3b = (n3 % l);
+	a1 = (n2 / limit);
+	a2 = (n2 % limit);
+	b1 = (n3 / limit);
+	b2 = (n3 % limit);
 
 	for (i = 92; i < 99; ++i)
 	{
-		printf(", %lu", n3a + (n3b / l));
-		printf("%lu", n3b % l);
-
-		n3a = n3a + n2a;
-		n2a = n3a - n2a;
-		n3b = n3b + n2b;
-		n2b = n3b - n2b;
+		printf(", %lu", b1 + (b2 / limit));
+		printf("%lu", b2 % limit);
+		b1 = b1 + a1;
+		a1 = b1 - a1;
+		b2 = b2 + a2;
+		a2 = b2 - a2;
+		
 	}
 	printf("\n");
 	return (0);
