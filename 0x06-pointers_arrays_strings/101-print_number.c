14cf2a1e20f5f0dbe5f8278e632a4506496aa8c2
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-22 17:06:26 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,28 +7,16 @@
  */
 void print_number(int n)
 {
-	unsigned int n1 = n, rev = 0, len = 0;
+	unsigned int n1 = n;
 
-	if (n == 0)
-		_putchar('0');
-
-	else if (n < 0)
+	if (n < 0)
 	{
 		_putchar('-');
 		n1 = -n;
 	}
 
-	while (n1 != 0)
-	{
-		rev = (rev * 10) + (n1 % 10);
-		n1 = n1 / 10;
-		++len;
-	}
-
-	while (len)
-	{
-		_putchar((rev % 10) + '0');
-		rev = rev / 10;
-		--len;
-	}
+	if(n1 / 10 != 0)
+		print_number(n1)
+	
+	_putchar((n1 % 10) + '0');
 }
