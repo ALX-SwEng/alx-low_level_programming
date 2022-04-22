a14f8a6f92d89a5e78d9ecfb8c0d8f3c25689420
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-22 21:10:25 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,36 +11,38 @@
 int main(void)
 {
 	unsigned long int i;
-	unsigned long int bef = 1;
-	unsigned long int aft = 2;
+	unsigned long int n1 = 0;
+	unsigned long int n2 = 1;
+	unsigned long int n3;
 	unsigned long int l = 1000000000;
-	unsigned long int bef1;
-	unsigned long int bef2;
-	unsigned long int aft1;
-	unsigned long int aft2;
-
-	printf("%lu", bef);
+	unsigned long int n2a;
+	unsigned long int n2b;
+	unsigned long int n3a;
+	unsigned long int n3b;
 
 	for (i = 1; i < 91; i++)
 	{
-		printf(", %lu", aft);
-		aft += bef;
-		bef = aft - bef;
+		n3 = n1 + n2;
+		printf(", %lu", n3);
+
+		n1 = n2;
+		n2 = n3;
 	}
 
-	bef1 = (bef / l);
-	bef2 = (bef % l);
-	aft1 = (aft / l);
-	aft2 = (aft % l);
+	n2a = (n2 / l);
+	n2b = (n2 % l);
+	n3a = (n3 / l);
+	n3b = (n3 % l);
 
 	for (i = 92; i < 99; ++i)
 	{
-		printf(", %lu", aft1 + (aft2 / l));
-		printf("%lu", aft2 % l);
-		aft1 = aft1 + bef1;
-		bef1 = aft1 - bef1;
-		aft2 = aft2 + bef2;
-		bef2 = aft2 - bef2;
+		printf(", %lu", n3a + (n3b / l));
+		printf("%lu", n3b % l);
+
+		n3a = n3a + n2a;
+		n2a = n3a - n2a;
+		n3b = n3b + n2b;
+		n2b = n3b - n2b;
 	}
 	printf("\n");
 	return (0);
