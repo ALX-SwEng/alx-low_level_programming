ab4dd3a59bf0287e07b7cffd189ea0341f9d7a23
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-22 17:23:43 +0300

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,16 +8,16 @@
 
 void print_number(int n)
 {
-	int n1 = n;
+	unsigned int n1 = n;
 
 	if (n < 0)
 	{
 		_putchar ('-');
-		n = -n1;
+		n1 = -n;
 	}
 	
-	if (n / 10 != 0)
-		print_number (n % 10);
+	if (n1 / 10 != 0)
+		print_number (n1 % 10);
 
-	_putchar ('0' + n % 10);
+	_putchar ('0' + n1 % 10);
 }
