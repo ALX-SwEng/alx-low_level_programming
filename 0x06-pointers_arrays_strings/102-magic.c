505a85f965b5dd764d73f40df46b431804182e9f
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-22 18:24:53 +0300

    Update 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
-/**
- * main - pointers arithmetic
- *
- * Return: Always 0.
- */
 int main(void)
 {
-   int *p;
-   int a[2];
-   int n;
+  int n;
+  int a[5];
+  int *p;
 
-   p = &n;
-   printf("p = &n;\np: %p\n", p);
-   printf("p + 1: %p\n", p + 5);
-   /* possible since a is evaluated */
-   /* as an int * in this context */
-   p = a;
-   printf("\na: %p\na +2: %p\n", a, a +2);
-   return (0);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
