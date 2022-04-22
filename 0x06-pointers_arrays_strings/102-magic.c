96807f8017256ec5f56c775bf3c40e945d8fe4a4
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-22 17:43:58 +0300

    Update 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,8 +16,8 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  *(p + 1) = 98;
+  *(p + 6) = 98;
   /* ...so that this prints 98\n */
-  printf("a[0] = %d\n", a[0]);
+  printf("a[3] = %d\n", a[3]);
   return (0);
 }
