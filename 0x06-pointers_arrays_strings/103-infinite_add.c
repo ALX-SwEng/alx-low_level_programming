fb5acaeb6a01ef410f4934620595496ce9eda30e
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-22 05:44:20 +0300

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -6,15 +6,10 @@
 
 void rev_string(char *n)
 {
-	int i = 0;
-	int j = 0;
+	int i, j = 0;
 	char temp;
 
-	while (*(n + i) != '\0')
-	{
-		i++;
-	}
-	i--;
+	for (i = -1; *(n + i) != '\0'; i++)
 
 	for (j = 0; j < i; j++, i--)
 	{
@@ -35,42 +30,45 @@ void rev_string(char *n)
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int overflow = 0, i = 0, j = 0, digits = 0;
+	int overflow = 0, i, j, digits = 0;
 	int val1 = 0, val2 = 0, temp_tot = 0;
 
-	while (*(n1 + i) != '\0')
-		i++;
-	while (*(n2 + j) != '\0')
-		j++;
-	i--;
-	j--;
+	for (i = -1; *(n1 + i) != '\0'; ++i);
+	for (j = -1; *(n2 + j) != '\0'; ++j);
+	
 	if (j >= size_r || i >= size_r)
 		return (0);
+
 	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-		if (i < 0)
-			val1 = 0;
-		else
-			val1 = *(n1 + i) - '0';
-		if (j < 0)
-			val2 = 0;
-		else
-			val2 = *(n2 + j) - '0';
+		val1 = *(n1 + i) - '0';
+		val2 = *(n2 + j) - '0';
+
 		temp_tot = val1 + val2 + overflow;
+
 		if (temp_tot >= 10)
+		{
 			overflow = 1;
+			temp_tot %= 10;
+		}
 		else
 			overflow = 0;
+
 		if (digits >= (size_r - 1))
 			return (0);
-		*(r + digits) = (temp_tot % 10) + '0';
+
+		*(r + digits) = (temp_tot + '0');
+
 		digits++;
 		j--;
 		i--;
 	}
+
 	if (digits == size_r)
 		return (0);
+
 	*(r + digits) = '\0';
 	rev_string(r);
+
 	return (r);
 }
