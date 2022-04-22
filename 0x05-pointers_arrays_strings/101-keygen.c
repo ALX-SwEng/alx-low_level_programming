12ecea6082740deb453f65db9a7b0e17ef8c9e72
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-22 22:38:35 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include <stdlib.h>  
 #include <time.h>
 
 /*
@@ -18,14 +17,14 @@ int main()
 
     for (i = 0; i < 100; i++) 
     {
-        pass[i] = rand() % 19;
+        pass[i] = rand() % 78;
 
 		sum += (pass[i] + '0');
 		putchar(pass[i] + '0');
 
-		if ((5050 - sum) - '0' < 19)
+		if ((2772 - sum) - '0' < 78)
 		{
-			n = 5050 - sum - '0';
+			n = 2772 - sum - '0';
 			sum += n;
 			putchar(n + '0');
 			break;
