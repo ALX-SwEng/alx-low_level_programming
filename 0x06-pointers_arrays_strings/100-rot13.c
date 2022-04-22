29a807edde2bce9b19790f0afec1962abf51015b
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-22 03:31:06 +0300

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,25 +9,20 @@
 
 char *rot13(char *str)
 {
-	int i = 0;
-	unsigned char s;
+	int i = 0, j;
 
-	while (*(str + i) != '\0')
+	char encode[] = {"ABCDEFGHILJKMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"};
+	char decode[] = {"NOPQRSTUVWXYZABCDEFGHILJKMnopqrstuvwxyzabcdefghijklm"};
+	
+	while (str[i] != '\0')
 	{
-		s = *(str + i);
-		if (s >= 'a' && s <= 'z')
+		for (j = 0; encode[j] != '\0'; ++j)
 		{
-			s += 13;
-			if (s > 'z')
-				s -= 26;
-			str[i] = s;
-		}
-		else if (s >= 'A' && s <= 'Z')
-		{
-			s += 13;
-			if (s > 'Z')
-				s -= 26;
-			str[i] = s;
+			 if (str[i] == encode[j])
+			 {
+			 	str[i] = decode[j];
+			 	break;
+			 }			 	
 		}
 		i++;
 	}
