15946d39e21bd08c0126829a5405d550835f9693
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-22 20:35:11 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdint.h>
 
 /**
  * main - print fibnonic series
@@ -11,18 +10,37 @@ int main(void)
 {
 	unsigned long int n1 = 0, n2 = 1, n3, i;
 
-	for (i = 1; i <= 98; ++i)
+	for (i = 1; i <= 91; ++i)
 	{
 		n3 = n1 + n2;
-
-		if (i < 93)
-			printf("%lu, ", n3);
-		else
-			printf("%lu", n3);
+		printf("%lu, ", n3);
 
 		n1 = n2;
 		n2 = n3;
 	}
+	
+	unsigned long int limit = 1000000000;
+	unsigned long int n2a;
+	unsigned long int n2b;
+	unsigned long int n3a;
+	unsigned long int n3b;
+	
+	n2a = (n2 / limit);
+	n2b = (n2 % limit);
+	
+	n3a = (n3 / limit);
+	n3b = (n3 % limit);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", n3a + (n3b / limit));
+		printf("%lu", n3b % limit);
+		n3a = n3a + n2a;
+		n2a = n3a - n2a;
+		n3b = n3b + n2b;
+		n2b = n3b - n2b;
+	}
+	
 	putchar('\n');
 
 	return (0);
