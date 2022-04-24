626b49fce5f7fccfeb8c25427f118dd2bdde8a19
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-24 10:33:14 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -34,7 +34,7 @@ int main(void)
 	for (i = 92; i < 99; ++i)
 	{
 		printf(", %lu", b1 + (b2 / limit));
-		printf("%lu", b2 % l);
+		printf("%lu", b2 % limit);
 		b1 = b1 + a1;
 		a1 = b1 - a1;
 		b2 = b2 + a2;
