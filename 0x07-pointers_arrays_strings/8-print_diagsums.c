97a05206e4b0dc7eba3d1bf67a09b7ef4da00e16
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-26 01:15:22 +0300

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,1 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagsums -  prints sums of square array diagonal
+ *
+ * @a: a pointer
+ * @size: size
+ */
+
 void print_diagsums(int *a, int size)
+{
+	int i, j, sumd1 = *a , sumd2 = 0;
+
+	for (i = 1; i <= size ; ++i)
+	{
+		if (i != size)
+		{
+			int d1 = (i * size) + i ;
+			sumd1 += *(a + d1);
+		}
+
+		int d2 = (size - 1) * i;		
+		sumd2 += *(a + d2);
+	}
+
+	printf("%d, ", sumd1);
+	printf("%d\n", sumd2);
+}
+
