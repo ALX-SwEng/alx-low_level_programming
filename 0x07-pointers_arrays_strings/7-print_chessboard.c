a8c8240c444ba44b3f82e0f52869d70cd678cca1
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-25 23:48:22 +0300

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,1 +1,24 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard.
+ * @a: array character
+ *
+ * Return: Always 0.
+ */
+
 void print_chessboard(char (*a)[8])
+{
+	int i, j;
+	
+	for (i = 0; i < 8; ++i)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			putchar(a[i][j]);
+			++j;
+		}
+		putchar('\n');
+	}
+}
