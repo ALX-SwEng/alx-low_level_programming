6adcf248464a69cf1e9c3e031ef3571a20a89c00
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-25 21:56:55 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,10 +13,13 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *pdest = dest, *psrc = src;
+	unsigned int i = 0;
 	
-	while (n-- > 0)
-		*pdest++ = *psrc++;
+	while (i < n)
+	{
+		dest[i] = src[i];
+		++i
+	}
 	
 return (dest);
 }
