6eadf7dea571e3caabfda05fc458a98685d2a3c0
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-25 22:14:30 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,19 +6,17 @@
  * @char: pointer to string
  * @c: character to be located
  *
- * Return: a pointer to the memory area
+ * Return: a pointer to the character
  */
 
 char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
-  {
-    if (*s == c)
-    {
-      return (*s);
-    }
+	{
+		if (*s == c)
+			return (*s);
 		++s;
-  }
+	}
 
 return (NULL);
 }
