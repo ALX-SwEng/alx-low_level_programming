70d8e4587fbc9157495ce81e4ef9ee1f07d24fb0
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-26 01:29:33 +0300

    Update 100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,1 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets pointer val to char
+ *
+ * @s: string double pointer
+ * @to: string pointer
+ */
+
 void set_string(char **s, char *to)
+{
+	*s = to;
+}
