65fe4678ae357a9556a92f0b411951b16317404c
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-25 23:20:56 +0300

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,1 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strpbrk -  searches a string for any of a set of bytes.
+ *
+ * @s: pointer to string
+ * @accept: substring to be located
+ *
+ * Return: a pointer to the byte in string
+ */
+
 char *_strpbrk(char *s, char *accept)
+{
+	if ((s == NULL) || (accept == NULL))
+		return (len);
+
+	 while(*s1)
+   {
+     if(strchr(accept, *s1))
+       return s1;
+     else
+       s++;
+	}
+
+return (NULL);
+}
