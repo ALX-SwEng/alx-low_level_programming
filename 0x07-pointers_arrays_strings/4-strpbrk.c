5c5531087c65800488956eb30e2612f76d326e6e
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-25 23:30:03 +0300

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -14,13 +14,17 @@ char *_strpbrk(char *s, char *accept)
 {
 	if ((s == NULL) || (accept == NULL))
 		return (NULL);
-		
-	while(*s)
+	
+	while (*s)
 	{
-		if(strchr(accept, *s))
+		if (strchr(accept, *s))
+		{
 			return (s);
+		}
 		else
+		{
 			s++;
+		}
 	}
 
 return (NULL);
