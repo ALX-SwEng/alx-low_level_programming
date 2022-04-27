c10f7685bb6fc11e59c4ee0f657baf689d71a67f
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-27 20:30:41 +0300

    Create 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * is_prime_number - check if the number is prime
+ *
+ * @n: integer number
+ *
+ * Return: 1 if number is prime, otherwise 0
+ */
+
+int is_prime_number(int n)
+{
+	return (is_prime(n, 2));
+}
+
+/**
+ * is_prime - check if the number is prime
+ *
+ * @n: integer number
+ * @i: integer, iterate until it is <n/2
+ *
+ * Return: 1 if number is prime, otherwise 0
+ */
+
+int is_prime(int n, int i)
+{
+	if (n <= 1)
+		return (0);
+
+	if (i <= n/2)
+	{
+		if (n % i == 0)
+			return (0);
+		
+		return (is_prime(n, ++i));
+	}
+	
+	return (1);
+}
