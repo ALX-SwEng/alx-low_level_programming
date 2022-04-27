3fb93096dd9d08843489dae58d71ed0b52ae04dc
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-27 14:11:57 +0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,14 +6,13 @@
  *
  * Return: nothing.
  */
-
-void void _print_rev_recursion(char *s)
+void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_putchar(*s);
-		_print_rev_recursion(++s);
+		_puts_recursion (s + 1);
+		putchar (*s);
 	}
-	else
-		_putchar('\n');
+	else	
+		putchar('\n');
 }
