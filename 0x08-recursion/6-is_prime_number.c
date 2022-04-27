fc9b8d5545244b63ca754dc61106bb7c2f0bab75
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-27 20:33:52 +0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -14,14 +14,14 @@ int is_prime(int n, int i)
 	if (n <= 1)
 		return (0);
 
-	if (i <= n/2)
+	if (i <= n / 2)
 	{
 		if (n % i == 0)
 			return (0);
-		
+
 		return (is_prime(n, ++i));
 	}
-	
+
 	return (1);
 }
 
