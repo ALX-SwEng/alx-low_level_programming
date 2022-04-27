5a2e105e6c0f7f9c9e9432cc4838507ddc12027a
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-27 14:15:14 +0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,9 +10,9 @@ void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_print_rev_recursion (s + 1);
-		_putchar (*s);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	else	
+	else
 		_putchar('\n');
 }
