8d19d8f12df2492e06ed0102c032a37a208858ee
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-27 10:22:31 +0300

    Create 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - recursively prints a string, followed by a new line.
+ * @s: string pointer
+ *
+ * Return: nothing.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		putchar (*s);
+		_puts_recursion (++s);
+	}
+	else	
+		putchar('\n');
+}
