05efbc42a4358763b261f90c04f975a2101e6f8e
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-29 22:29:21 +0300

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,10 +8,10 @@
 * Return: Always success.
 */
 
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
 	if (argc > 0)
-		printf("%d\n", argc - 1);
+		printf("%d\n", argc);
 
 return (0);
 }
