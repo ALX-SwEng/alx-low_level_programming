f788b2baddab50bffa221283832e51bdf9d4b9fa
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-29 23:43:34 +0300

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - prints name name of program.
+* main - prints the number of arguments passed to command line.
 * @argc: number of command line arguments
 * @argv: An array containing the program command line arguments
 *
