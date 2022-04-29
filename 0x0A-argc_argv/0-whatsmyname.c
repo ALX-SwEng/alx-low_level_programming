712602c11e9d6ccf161b861c3310aebc91b9bc8b
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-29 23:42:01 +0300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - prints name name of program.
+* main - prints name of program.
 * @argc: number of command line arguments
 * @argv: An array containing the program command line arguments
 *
