a2cc0a7f76b34158c67c37337a0464af40da552c
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-30 00:15:38 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,26 +3,6 @@
 #include <string.h>
 
 /**
-* isNumber - check if string is number.
-* @str: string parameter
-*
-* Return: 0 if number otherwise 0.
-*/
-
-int isNumber(char *str)
-{
-	int j = strlen(str);
-
-	while (j--)
-	{
-		if (str[j] > 47 && str[j] < 58)
-			continue;
-		return (0);
-	}
-return (1);
-}
-
-/**
 * main - prints name name of program.
 * @argc: number of command line arguments
 * @argv: An array containing the program command line arguments
@@ -32,30 +12,51 @@ return (1);
 
 int main(int argc, char *argv[])
 {
-	int i, sum = 0, count = 0;
+	int change, c25 = 0, c10 = 0, c5 = 0, c2 = 0, c1 = 0;
 
-	if (argc - 1 > 0)
-	{
-		for (i = 1; i < argc; ++i)
-		{
-			if (!isNumber(argv[i]))
-				++count;
-			else
-				sum += atoi(argv[i]);
-		}
-	}
-
-	if (argc - 1 == 0 || count == argc - 1)
-		printf("0\n");
-
-	else if (count > 0)
+	if (argc - 1 != 1)
 	{
 		printf("Error\n");
-		return (1);
+		return (1);		
 	}
 
+	if (atoi(argv[1]) < 0)
+		printf("0\n");
 	else
-		printf("%d\n", sum);
+	{
+		change = atoi(argv[1]);
+		while (change > 0)
+		{
+			if (change >= 25)
+			{
+				c25 = change / 25;
+				change = change % 25;
+			}
+
+			if (change >= 10)
+			{
+				c10 = change / 10;
+				change = change % 10;
+			}
+
+			if (change >= 5)
+			{
+				c5 = change / 5;
+				change = change % 5;
+			}
+
+			if (change >= 2)
+			{
+				c2 = change / 2;
+				change = change % 2;
+			}
+			
+			if (change >= 1)
+				c1 = change;;
+		}
+
+	printf("%d\n", c25 + c10 + c5 + c2 + c1);
+	}
 
 return (0);
 }
