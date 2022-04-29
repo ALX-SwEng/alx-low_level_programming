81c0dbd0a9e25452069038d1832f3718e01b01f9
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-29 23:41:28 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,61 @@
 #include <stdio.h>
-int main (int argc, char *argv[])
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* isNumber - check if string is number.
+* @str: string parameter
+*
+* Return: 0 if number otherwise 0.
+*/
+
+int isNumber(char *str)
 {
-	printf (argv[0]);
-  return 0;
+	int j = strlen(str);
+
+	while (j--)
+	{
+		if (str[j] > 47 && str[j] < 58)
+			continue;
+		return (0);
+	}
+return (1);
+}
+
+/**
+* main - prints name name of program.
+* @argc: number of command line arguments
+* @argv: An array containing the program command line arguments
+*
+* Return: Always success.
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0, count = 0;
+
+	if (argc - 1 > 0)
+	{
+		for (i = 1; i < argc; ++i)
+		{
+			if (!isNumber(argv[i]))
+				++count;
+			else
+				sum += atoi(argv[i]);
+		}
+	}
+
+	if (argc - 1 == 0 || count == argc - 1)
+		printf("0\n");
+
+	else if (count > 0)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	else
+		printf("%d\n", sum);
+
+return (0);
 }
