7090401bc596e83ce160674aa8c939ba13594294
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-30 00:34:36 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -16,33 +16,34 @@ int main(int argc, char *argv[])
 	if (argc - 1 != 1)
 	{
 		printf("Error\n");
-		return (1); }
+		return (1); 
+	}
 
 	if (atoi(argv[1]) < 0)
 		printf("0\n");
 	else
 	{
 		change = atoi(argv[1]);
-		while (change > 0)
+		if (change >= 25)
 		{
-			if (change >= 25)
-			{
-				c25 = change / 25;
-				change = change % 25; }
-			if (change >= 10)
-			{
-				c10 = change / 10;
-				change = change % 10; }
-			if (change >= 5)
-			{
-				c5 = change / 5;
-				change = change % 5; }
-			if (change >= 2)
-			{
-				c2 = change / 2;
-				change = change % 2; }
-			if (change >= 1)
-				c1 = change;
-		}
-	printf("%d\n", c25 + c10 + c5 + c2 + c1); }
-return (0); }
+			c25 = change / 25;
+			change = change % 25; }
+		if (change >= 10)
+		{
+			c10 = change / 10;
+			change = change % 10; }
+		if (change >= 5)
+		{
+			c5 = change / 5;
+			change = change % 5; }
+		if (change >= 2)
+		{
+			c2 = change / 2;
+			change = change % 2; }
+		if (change >= 1)
+			c1 = change;
+		
+		printf("%d\n", c25 + c10 + c5 + c2 + c1);
+	}
+return (0); 
+}
