ec7eb9e1709c78cd3971cea141f7a9ae6a89eaf2
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-29 23:19:55 +0300

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
-int main (int argc, char *argv[])
+#include <stdlib.h>
+
+/**
+* main - prints name name of program.
+* @argc: number of command line arguments
+* @argv: An array containing the program command line arguments
+*
+* Return: Always success.
+*/
+
+int main(int argc, char *argv[])
 {
-	printf (argv[0]);
-  return 0;
+	int i, sum = 0, count = 0;
+
+	if (argc - 1 > 0)
+	{
+		for (i = 1; i < argc; ++i)
+		{
+			if (!isnumber(argv[i]))
+				++count;
+			else
+				sum += atoi (argv[i]);
+		}
+	}
+
+	if (argc - 1 == 0 || count == argc - 1)
+		printf("0\n");
+
+	else if (count > 1)
+	{
+		printf("Error\n");
+		return (1)
+	}
+	
+	else		 
+		printf("%d\n", sum);
+
+return (0);
 }
