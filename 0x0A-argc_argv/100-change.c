3e192160766b31e6eb1e304e3a3b5d98b8815466
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-30 00:30:30 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
 * main - prints name name of program.
@@ -17,8 +16,7 @@ int main(int argc, char *argv[])
 	if (argc - 1 != 1)
 	{
 		printf("Error\n");
-		return (1);
-	}
+		return (1); }
 
 	if (atoi(argv[1]) < 0)
 		printf("0\n");
@@ -30,27 +28,21 @@ int main(int argc, char *argv[])
 			if (change >= 25)
 			{
 				c25 = change / 25;
-				change = change % 25;
-			}
+				change = change % 25; }
 			if (change >= 10)
 			{
 				c10 = change / 10;
-				change = change % 10;
-			}
+				change = change % 10; }
 			if (change >= 5)
 			{
 				c5 = change / 5;
-				change = change % 5;
-			}
+				change = change % 5; }
 			if (change >= 2)
 			{
 				c2 = change / 2;
-				change = change % 2;
-			}
+				change = change % 2; }
 			if (change >= 1)
 				c1 = change;
 		}
-	printf("%d\n", c25 + c10 + c5 + c2 + c1);
-	}
-return (0);
-}
+	printf("%d\n", c25 + c10 + c5 + c2 + c1); }
+return (0); }
