606eeef6d5172cfd165db670301ac0091441f7c5
Chere Lemma <chere.lemma@aastu.edu.et>
2022-04-29 23:03:12 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
 * main - prints name name of program.
@@ -16,7 +17,7 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	else
-		printf("%d\n", atoi(argv[1]) * aoti(argv[2]));
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
 return (0);
 }
