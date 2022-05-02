6bd3466ea61044929fc51744be8cc9adbcf94365
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-03 00:24:08 +0300

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	newArray = malloc(height * sizeof(int));
+	newArray = malloc(height * sizeof(int*));
 	if (newArray == NULL)
 		return (NULL);
 
