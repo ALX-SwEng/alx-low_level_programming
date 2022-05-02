5d561bd1d4e99f1e11b5b3307d1d5f37b0b37547
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-02 22:40:39 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,16 +15,16 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *newstr;
-	unsigned int i, j = 0; size, size1 = 0, size2 = 0;
+	unsigned int i, j = 0, size, size1 = 0, size2 = 0;
 
 	if (!s1)
-			*s1 = '';
+		*s1 = '';
 	if (!s2)
-			*s2 = '';
-  
+		*s2 = '';
+
 	while (*(s1 + size1++))
 		;
-  while (*(s2 + size2++))
+	while (*(s2 + size2++))
 		;
 
 	newstr = malloc((size1 + size2 - 1) * sizeof(char));
@@ -36,8 +36,8 @@ char *str_concat(char *s1, char *s2)
 	{
 		for (i = 0; i < size1 - 1; ++i, ++j)
 			newstr[j] = s1[i];
-
-    for (i = 0; i < size2; ++i, ++j)
+		
+		for (i = 0; i < size2; ++i, ++j)
 			newstr[j] = s2[i];
 	}
 
