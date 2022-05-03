a9449b499eb2e03e04ca207f54e0e3159f7dfe10
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-03 03:17:53 +0300

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -18,7 +18,7 @@ char **strtow(char *str)
 
 	while (*(str + i))
 	{
-		if (*(str + i) == ' ' && *(str + i + 1) != ' ' && *(str + i + 1) != '\0')
+		if (*(str + i) == ' ' && (*(str + i + 1) != ' ' || *(str + i + 1) != '\0'))
 			wc++;
 		++i;
 	}
