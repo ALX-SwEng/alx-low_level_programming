ce16c019303530acf7cdbb85850c377770f1b0ed
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-06 15:48:32 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,7 +14,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *newstr, *temp;
-	unsigned int size, size1 = 0, size2 = 0;
+	unsigned int size, size1 = 0, size2 = 0, i = 0;
 
 	if (!s1)
 		s1 = "";
@@ -38,7 +38,7 @@ char *str_concat(char *s1, char *s2)
 	while (*s1)
 		*temp++ = *s1++;
 
-	while (*s2)
+	while (i++ < size)
 		*temp++ = *s2++;
 
 return (newstr);
