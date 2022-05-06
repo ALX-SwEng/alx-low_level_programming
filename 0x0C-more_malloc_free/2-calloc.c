87a83b0f243f677acad043a80c7f1d15a9d3b28c
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-06 17:14:44 +0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,1 +1,43 @@
+#include <stdlib.h>
+#include "main.h"
 
+/**
+ * _memset - copy char
+ * @s: string
+ * @b: input
+ * @n: bytes
+ * Return: string
+ */
+
+char *_memset(char *str, char ch, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+		str[i++] = ch;
+
+	return (str);
+}
+
+/**
+* malloc_checked - allocates memory using malloc.
+*
+* @b: size of the pointer to be created.
+*
+* Return:  pointer to the allocated memory, or 98 if it fails.
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+  void *ptr;
+ 
+  if (size <= 0 || nmemb == 0)
+   return (0);
+ 
+	ptr = malloc(nmemb * size);
+	if (!ptr)
+    return (0);
+
+
+	return (_memset(ptr, 0, nmemb * size));
+}
