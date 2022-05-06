8cddd9c82d5e7886a2dbfbcfe6a7e2f077f3f742
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-06 16:17:17 +0300

    Update main.c

--- 0x0C-more_malloc_free/main.c
+++ 0x0C-more_malloc_free/main.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
 /**
  * main - check the code
@@ -10,22 +9,10 @@
  */
 int main(void)
 {
-    char *c;
-    int *i;
-    float *f;
-    double *d;
+    char *concat;
 
-    c = malloc_checked(sizeof(char) * 1024);
-    printf("%p\n", (void *)c);
-    i = malloc_checked(sizeof(int) * 402);
-    printf("%p\n", (void *)i);
-    f = malloc_checked(sizeof(float) * 100000000);
-    printf("%p\n", (void *)f);
-    d = malloc_checked(INT_MAX);
-    printf("%p\n", (void *)d);
-    free(c);
-    free(i);
-    free(f);
-    free(d);
+    concat = string_nconcat("Best ", "School !!!", 6);
+    printf("%s\n", concat);
+    free(concat);
     return (0);
 }
