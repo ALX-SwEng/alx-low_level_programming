dbf2760063f0dec2aa6b024175b7f438cf03d166
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-07 10:28:12 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n > size2)
 		n = size2;
 
-	size = size1 + n;
+	size = size1 + n + 1;
 	newstr = malloc(size * sizeof(char));
 
 	if (!newstr)
