411c5ea8476df402ca181f8898c1d344c82b081a
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-07 10:14:38 +0300

    Update main.c

--- 0x0C-more_malloc_free/main.c
+++ 0x0C-more_malloc_free/main.c
@@ -1,18 +1,24 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
+char *string_nconcat(char *, char *, unsigned int);
+
 /**
- * main - check the code
+ * main - check the code .
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char *concat;
+	char *s;
 
-    concat = string_nconcat("Best ", "School !!!", 6);
-    printf("%s\n", concat);
-    free(concat);
-    return (0);
+	s = string_nconcat("Hello", "", 12);
+	if (s == NULL)
+	{
+		printf("failed\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
+	return (0);
 }
