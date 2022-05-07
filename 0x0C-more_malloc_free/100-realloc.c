3138574acdf7ca597f3797bdd1f2a6b879259f2a
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-08 00:56:58 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,7 +25,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
-		return (ptr);
+		return (NULL);
 	}
 
 	reallocPtr = malloc(new_size);
