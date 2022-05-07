112e53afbb3a66e52edf21396c0a01e18e8a67e8
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-07 10:20:04 +0300

    Update main.c

--- 0x0C-more_malloc_free/main.c
+++ 0x0C-more_malloc_free/main.c
@@ -20,5 +20,31 @@ int main(void)
 	}
 	printf("%s\n", s);
 	free(s);
+	
+	s = string_nconcat("Hello", NULL, 12); 
+	if (s == NULL)
+	{
+		printf("failed\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
+	
+	s = string_nconcat(NULL, "Hello", 0); 
+	if (s == NULL)
+	{
+		printf("failed\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
+	s = string_nconcat(NULL, NULL, 10);
+	if (s == NULL)
+	{
+		printf("failed\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
 	return (0);
-}	
+}
