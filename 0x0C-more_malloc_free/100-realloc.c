b131b604bf2e0ff74575fccd48beac2c8895aaef
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-07 22:02:34 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,1 +1,42 @@
+#include <stdlib.h>
+#include "main.h"
 
+/**
+* _realloc - reallocates a memory block using malloc and free.
+*
+* @old_size: the size, in bytes, of the allocated space for ptr.
+* @new_size: the new size, in bytes of the new memory block.
+*
+* Return: pointer to the newly created array, or NULL if it fails.
+*/
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+  void *newPtr;
+  int i = 0;
+
+	if (old_size > new_size)
+		return (ptr);
+
+	if (new_size == 0 && !ptr)
+  {
+    free(ptr)
+    return (ptr);
+  }
+  
+  if (ptr == NULL)
+  {
+    newPtr = malloc(new_size);
+    if (newPtr == NULL)
+    {
+          free(ptr)
+          return (NULL);
+    }
+    else 
+    {
+      while (i < old_size)
+        newPtr[i++] = *ptr++
+      return (newPtr);
+    }
+  }
+}
