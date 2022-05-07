1524088f8387c5d7d272bb6debfc31f37db6ab94
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-07 20:33:42 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *newstr, *temp;
-	unsigned int size, size1 = 0, size2 = 0, i = 0;
+	unsigned int size, size1 = 0, size2 = 0;
 
 	if (!s1)
 		s1 = "";
