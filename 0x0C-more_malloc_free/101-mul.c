a264fcba95ff731133c8f1328fab80ea9d1893cb
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-08 18:10:53 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,92 +1,18 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-/**
-  * int_calloc - special calloc but 4 int arrays
-  * @nmemb: n memb
-  * @size: size of array
-  * Return: int *
-  */
-int *int_calloc(int nmemb, unsigned int size)
-{
-	/* declarations */
-	int *p, n;
-	/* checking inputs */
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-	/* malloc the space & check for fail */
-	p = malloc(nmemb * size);
-	if (p == NULL)
-		return (NULL);
-	/* calloc */
-	for (n = 0; n < nmemb; n++)
-		p[n] = 0;
-	return (p);
-}
+#include <string.h>
+#include "main.h"
+
+int isNumber(char *str);
+int *int_calloc(int nmemb, unsigned int size);
+void mult(int *product, char *n1, char *n2, int len1, int len2);
 
 /**
-  * mult - multiplication
-  * @product: int * 4 answer
-  * @n1: string num1
-  * @n2: string num2
-  * @len1: len num1
-  * @len2: len num2
+  * error - print error message.
+  * @code: error code for exit
   * Return: void
   */
-void mult(int *product, char *n1, char *n2, int len1, int len2)
-{
-	/* declarations */
-	int i;
-	int j;
-	int f1, f2;
-	int sum;
-	/* the long math */
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		sum = 0;
-		f1 = n1[i] - '0';
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			f2 = n2[j] - '0';
-			sum += product[i + j + 1] + (f1 * f2);
-			product[i + j + 1] = sum % 10;
-			sum /= 10;
-		}
-		if (sum > 0)
-			product[i + j + 1] += sum;
-	}
-	for (i = 0; product[i] == 0 && i < len1 + len2; i++)
-	{}
-	if (i == len1 + len2)
-		_putchar('0');
-	for (; i < len1 + len2; i++)
-		_putchar(product[i] + '0');
-	_putchar('\n');
-}
-
-/**
-  * is_valid - is the number a valid one
-  * @num : char string num
-  * Return: int, 1 if true 0 if false
-  */
-int is_valid(char *num)
-{
-	/* declarations */
-	int i;
-	/* checking for ints */
-	for (i = 0; num[i]; i++)
-	{
-		if (num[i] < '0' || num[i] > '9')
-			return (0);
-	}
-	return (1);
-}
-/**
-  * err - errors r us
-  * @status: error code 4 exit
-  * Return: void
-  */
-void err(int status)
+void error(int code)
 {
 	_putchar('E');
 	_putchar('r');
@@ -94,44 +20,140 @@ void err(int status)
 	_putchar('o');
 	_putchar('r');
 	_putchar('\n');
-	exit(status);
+	exit(code);
 }
+
 /**
-  * main - getting the args
-  * @argc: args #
-  * @argv: arg array
-  * Return: 0
-  */
-int main(int argc, char **argv)
+* main - multiplies two numbers recieved through command line.
+* @argc: number of command line arguments
+* @argv: An array containing the program command line arguments
+*
+* Return: 0 if success otherwise 1.
+*/
+
+int main(int argc, char *argv[])
 {
-	/* declarations */
-	int i, j, len1 = 0, len2 = 0;
-	int *res;
-	/* too many args? too few? */
-	if (argc != 3)
+	int *mul, i, j, len1 = 0, len2 = 0;
+
+	if (argc - 1 != 2)
+		error(98);
+
+	for (i = 1; i < argc; ++i)
 	{
-		err(98);
-	}
-	/* using isvalid */
-	for (i = 1; i < argc; i++)
-	{
-		if (!(is_valid(argv[i])))
-			err(98);
+		if (!isNumber(argv[i]))
+			error(98);
 		if (i == 1)
 		{
 			for (j = 0; argv[i][j]; j++)
-				len1++;
+				++len1;
 		}
 		if (i == 2)
 		{
 			for (j = 0; argv[i][j]; j++)
-				len2++;
+				++len2;
 		}
 	}
-	res = int_calloc(len1 + len2, sizeof(int));
-	if (res == NULL)
-		err(98);
-	mult(res, argv[1], argv[2], len1, len2);
-	free(res);
-	return (0);
+
+	mul = int_calloc(len1 + len2, sizeof(int));
+	if (mul == NULL)
+		error(98);
+
+	mult(mul, argv[1], argv[2], len1, len2);
+	free(mul);
+return (0);
+}
+
+/**
+* isNumber - check if string is number.
+* @str: string parameter
+*
+* Return: 1 if number otherwise 0.
+*/
+
+int isNumber(char *str)
+{
+	int j = strlen(str);
+
+	while (j--)
+	{
+		if (str[j] > 47 && str[j] < 58)
+			continue;
+		return (0);
+	}
+return (1);
+}
+
+/**
+  * int_calloc - special calloc for int arrays
+  * @nmemb: n memb
+  * @size: size of array
+  * Return: int *
+  */
+int *int_calloc(int nmemb, unsigned int size)
+{
+	int *p, n;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	/* malloc the space & check for fail */
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+
+	for (n = 0; n < nmemb; n++)
+		p[n] = 0;
+
+	return (p);
+}
+
+/**
+  * mult - perform multiplication
+  *
+  * @product: int pointer for mul answer
+  * @n1: num1 as a string param
+  * @n2: num2 as a string param
+  * @len1: len of num1
+  * @len2: len of num2
+  *
+  * Return: void
+  */
+
+void mult(int *product, char *n1, char *n2, int len1, int len2)
+{
+	int i, j, i_n1 = 0, i_n2 = 0, res1, res2, sum, carry;
+
+	/* the long math */
+	/* Go from right to left in num1 */
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		sum = 0;
+		carry = 0;
+		i_n2 = 0;
+		res1 = n1[i] - '0';
+
+		/* Go from right to left in num2 */
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			res2 = n2[j] - '0';
+			sum = (res1 * res2) + product[i_n1 + i_n2] + carry;
+			 product[i_n1 + i_n2] = sum % 10;
+			carry = sum / 10;
+			i_n2++;
+		}
+
+		if (carry > 0)
+			product[i_n1 + i_n2] += carry;
+		i_n1++;
+	}
+	i = len1 + len2;
+	while (--i > 0 && product[i] == 0)
+		;
+
+	if (i == -1)
+		_putchar('0');
+
+	while (i >= 0)
+		_putchar(product[i--] + '0');
+	_putchar('\n');
 }
