72c8ee665b3f89e8675cc75ea124efe9c02b4a38
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-08 03:27:56 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -72,13 +72,13 @@ void mult(int *product, char *n1, char *n2, int len1, int len2)
 		{
 			res2 = n2[j];
 			sum += product[i + j + 1] + (res1 * res2);
-			_putchar(sum + '0');
+			_putchar(sum);
 			product[i + j + 1] = sum % 10;
 			sum /= 10;
 		}
 		if (sum > 0)
 			product[i + j + 1] += sum;
-		_putchar(product[i + j + 1]);
+		_putchar(product[i + j + 1] + '0');
 		_putchar('\n');
 	}
 	for (i = 0; product[i] == 0 && i < len1 + len2; i++)
