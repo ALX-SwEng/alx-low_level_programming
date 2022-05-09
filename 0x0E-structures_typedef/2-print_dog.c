4c2846cbd5c0ca30f94c4e7079ef8d2c6a2eaeb2
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-09 11:59:39 +0300

    Create 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+* int_dog - initialize dog structure
+*
+* @d: structure object
+* @name: 1st member 
+* @age: 2nd member 
+* @owner: 3rd member 
+*/
+
+oid print_dog(struct dog *d)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
