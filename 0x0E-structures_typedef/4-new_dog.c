e1b8a19eacbad11918a0850c0961d0406cf33971
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-09 12:19:20 +0300

    Create 4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+
+/**
+* new_dog - create new dog object
+*
+* @name: 1st member 
+* @age: 2nd member 
+* @owner: 3rd member 
+*/
+
+void new_dog (char *name, float age, char *owner)
+{
+  struct dog *newDog;
+
+  newDog = malloc (sizeof(struct dog));
+  if (!newDog)
+    return (NULL);
+  
+  newDog->name = name;
+  newDog->age = age;
+  newDog->owner = owner;
+}
