23c4c6b636aa68ae6e2e50421556955113390cf3
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-09 12:31:10 +0300

    Update main.c

--- 0x0E-structures_typedef/main.c
+++ 0x0E-structures_typedef/main.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "dog.h"
 
 /**
@@ -12,5 +11,6 @@ int main(void)
 
     my_dog = new_dog("Poppy", 3.5, "Bob");
     printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+    free_dog(my_dog);
     return (0);
 }
