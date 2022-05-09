c9826c441b165ea3ec892dcba8771469ba2e29a8
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-09 13:04:29 +0300

    Update main.c

--- 0x0E-structures_typedef/main.c
+++ 0x0E-structures_typedef/main.c
@@ -8,10 +8,9 @@
  */
 int main(void)
 {
-    dog_t *my_dog;
+    struct dog my_dog;
 
-    my_dog = new_dog("Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
-    free_dog(my_dog);
+    init_dog(&my_dog, "Poppy", 3.5, "Bob");
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
     return (0);
 }
