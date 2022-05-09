94dd680cec4815d7a56a775ec451f78e5c53bf91
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-09 12:15:56 +0300

    Update main.c

--- 0x0E-structures_typedef/main.c
+++ 0x0E-structures_typedef/main.c
@@ -13,6 +13,6 @@ int main(void)
     my_dog.name = "Poppy";
     my_dog.age = 3.5;
     my_dog.owner = "Bob";
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+    print_dog(&my_dog);
     return (0);
 }
