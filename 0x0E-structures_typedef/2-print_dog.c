02e5342e5b3721fdc20be7b15152b17ea9252e4e
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-09 12:58:35 +0300

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,7 +17,7 @@ void print_dog(struct dog *d)
 		else
 			printf("Name: %s\n", d->name);
 
-		printf("age: %0.6f\n", d->age);
+		printf("Age: %0.6f\n", d->age);
 
 		if (d->owner == NULL)
 			printf("Owner: (nil)\n");
