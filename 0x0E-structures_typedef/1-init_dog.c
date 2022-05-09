ff09b01cae908aa1a3220d0ceede3c07c24422f2
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-09 11:54:20 +0300

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,9 +4,9 @@
 * int_dog - initialize dog structure
 *
 * @d: structure object
-* @name: 1st member 
-* @age: 2nd member 
-* @owner: 3rd member 
+* @name: 1st member
+* @age: 2nd member
+* @owner: 3rd member
 */
 
 void int_dog (struct dog *d, char *name, float age, char *owner)
