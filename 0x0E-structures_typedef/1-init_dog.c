aecb4ac116d1ab44ac3657eb9665d1218227b540
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-09 11:54:58 +0300

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,7 @@
 * @owner: 3rd member
 */
 
-void int_dog (struct dog *d, char *name, float age, char *owner)
+void int_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
 	d->age = age;
