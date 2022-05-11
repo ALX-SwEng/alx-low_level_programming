0dd011efc517356da4217ce787337285258a3087
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-11 11:01:42 +0300

    Create 21-int_index.c

---
+++ 0x0F-function_pointers/21-int_index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - search integer value in array
+ *
+ * @int_index: array pointer
+ * @size: array size
+ * @cmp: pointer to function that compare values
+ *
+ * Return: 0 if success, fails (-1) otherwise.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	size_t i;
+
+	if (size <= 0)
+		return(-1);
+	
+	for (i = 0; i < size; ++i)
+	{
+		if(cmp(array[i]))
+			return (0);
+	}
+
+return (-1)
+}
