57bd992c1d4b6895edeb068b268436812ebfbb53
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-11 09:24:06 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "calc.h"
 
+/**
+ * main - validate input number and operator
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+  int num1, num2;
+
+	if (argc - 1 != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	
+	if (argv[2] != '+' || *argv[2] != '-' || *argv[2] != '*' || 
+      *argv[2] != '/' || *argv[2] != '%')
+	{
+    	printf("Error\n");
+    	exit(98);
+  	}
+  
+return (0);
+}
