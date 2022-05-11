b80cca8fb104fdbfba31049b050824fa6b167418
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-11 11:31:41 +0300

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,22 @@
 #include "calc.h"
 
-int (*get_op_func(char *s))(int, int)
+int (*get_op_func(char *s))(int num1, int num2)
 {
-	while
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	while (i++ < 5)
+	{
+		if (*s == ops[i].op[0])
+			return (ops[i].f(num1, num2))
+	}
+
+return (NULL);
 }
