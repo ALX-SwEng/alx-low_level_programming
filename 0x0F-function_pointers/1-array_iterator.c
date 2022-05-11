ad20aa0391b89114fdc215e23aff5d093e20c2e3
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-11 10:46:22 +0300

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include "function_pointers.h"
 
+/**
+ * array_iterator - call a function that iterate over an array
+ *
+ * @size: array size
+ * @action: pointer to function
+ *
+ * Return: Nothing.
+ */
+ 
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; ++i)
+		action (array[i]);
+}
