c9585b757d72ca392a23c8c91e272de61b23de58
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-11 08:44:40 +0300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,14 @@
+#include "function_pointers.h"
 
+/**
+ * is_98 - check if a number is equal to 98
+ * @name: string param
+ * @f: pointer to function
+ *
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+  f(name);
+}
