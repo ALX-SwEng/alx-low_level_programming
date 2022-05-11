2b2bd15bd0cb58eb54bdc79c9eb2eb8073b644ac
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-11 09:29:16 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -5,6 +5,9 @@
 /**
  * main - validate input number and operator
  *
+ * @argc: number of command line arguments
+ * @argv: An array containing the program command line arguments
+ *
  * Return: Always 0.
  */
 
@@ -17,8 +20,8 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	
-	if (argv[2] != '+' || *argv[2] != '-' || argv[2] != '*' || 
+
+	if (argv[2] != '+' || *argv[2] != '-' || argv[2] != '*' ||
 	    argv[2] != '/' || argv[2] != '%')
 	{
 		printf("Error\n");
@@ -27,7 +30,7 @@ int main(int argc, char *argv[])
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	
+
 	if ((argv[2] != '/' || argv[2] != '%') && argv[3] == 0)
 	{
 		printf("Error\n");
