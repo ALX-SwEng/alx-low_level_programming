b4adee608ea8719f2df7c1bef5d0b6936a50b218
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-11 18:22:53 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
-	if ((argv[2] != '/' || argv[2] != '%') && argv[3] == 0)
+	if ((*argv[2] != '/' || *argv[2] != '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
