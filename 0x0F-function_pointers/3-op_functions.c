908200f9ede1340945d588382cdcb9891f24a981
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-11 11:49:47 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,22 +1,62 @@
+/**
+* op_add - add two numbers 
+* @a: 1st number, integer
+* @b: 2nd number, integer
+*
+* Return: sum
+*/
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
+/**
+* op_sub - find difference of two number 
+* @a: 1st number, integer
+* @b: 2nd number, integer
+*
+* Return: difference
+*/
 
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
+/**
+* op_mul - multiply two number 
+* @a: 1st number, integer
+* @b: 2nd number, integer
+*
+* Return: product
+*/
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
+/**
+* op_div - divide 1st number by the 2nd  
+* @a: 1st number, integer
+* @b: 2nd number, integer
+*
+* Return: quotient 
+*/
+
 int op_div(int a, int b)
 {
 	  return (a / b);
 }
+
+/**
+* op_mod - find reminder  
+* @a: 1st number, integer
+* @b: 2nd number, integer
+*
+* Return: reminder
+*/
 
 int op_mod(int a, int b)
 {
