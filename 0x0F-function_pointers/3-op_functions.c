d62b2704545174002eaf948102c4a4765d17cb5c
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-11 08:59:24 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,24 +1,24 @@
 int op_add(int a, int b)
 {
-		
+	return (a + b);
 }
 
 int op_sub(int a, int b)
 {
-	 
+	return (a - b);
 }
 
 int op_mul(int a, int b)
 {
-	  
+	return (a * b);
 }
 
 int op_div(int a, int b)
 {
-	  
+	  return (a / b);
 }
 
 int op_mod(int a, int b)
 {
-	  
+	  return (a % b);
 }
