d7ab0c6bd3ad954c0a10da8c44113811dc6a7c8b
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-11 14:36:32 +0300

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,9 +15,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (size > 0)
+	if (size > 0 && action)
 	{
 		for (i = 0; i < size; ++i)
-		action(array[i]);
+			action(array[i]);
 	}
 }
