fab4773f26c5e8e0be37172c852bc26dcddcb8d5
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-11 08:53:58 +0300

    Update 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,1 +1,12 @@
+#ifndef _CALC_
+#define _CALC_
 
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+int (*get_op_func(char *s))(int, int);
+
+#endif
