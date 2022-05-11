b6eb9e876b35a3a986189e5b2fbc02a05ae314cc
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-11 14:35:17 +0300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,17 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
- * @name: name's main
- * @f: void function
+ * print_name - print a name
+ * @name: string param
+ * @f: pointer to function
+ *
+ * Return: Nothing.
  */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
 	{
-		f(name);
+		(*f)(name);
 	}
 }
-
