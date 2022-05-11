be80d06040b5a2f58561262bc97c6c90858c545f
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-11 09:27:55 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-  int num1, num2;
+	int num1, num2;
 
 	if (argc - 1 != 3)
 	{
@@ -18,12 +18,21 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	
-	if (argv[2] != '+' || *argv[2] != '-' || *argv[2] != '*' || 
-      *argv[2] != '/' || *argv[2] != '%')
+	if (argv[2] != '+' || *argv[2] != '-' || argv[2] != '*' || 
+	    argv[2] != '/' || argv[2] != '%')
 	{
-    	printf("Error\n");
-    	exit(98);
-  	}
-  
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	
+	if ((argv[2] != '/' || argv[2] != '%') && argv[3] == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
 return (0);
 }
