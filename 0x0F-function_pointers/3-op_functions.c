2c77e6c3431bacc60a55a5a83bfb1e038f63d493
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-11 18:48:54 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -47,7 +47,11 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
-	return (a / b);
+	if (b != 0)
+		return (a / b);
+
+	printf("Error\n");
+	exit(100);
 }
 
 /**
@@ -60,5 +64,9 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
-	return (a % b);
+	if (b != 0)
+		return (a % b);
+
+	printf("Error\n");
+	exit(100);
 }
