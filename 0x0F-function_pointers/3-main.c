d2950115dfa5317167416f9152bc9f65e0b14784
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-11 18:47:24 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -30,12 +30,6 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
-	if ((*argv[2] != '/' || *argv[2] != '%') && num2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	func = get_op_func(argv[2]);
 
 	if (!func)
