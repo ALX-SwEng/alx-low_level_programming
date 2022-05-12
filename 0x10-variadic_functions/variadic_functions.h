fb3e90447077c50a27cc58cbf9a559d3126f49fd
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-12 15:26:52 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,8 @@
 #ifndef FILE_VARIADIC_FUNCTIONS
 #define FILE_VARIADIC_FUNCTIONS
 
+int _putchar(char c);
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
