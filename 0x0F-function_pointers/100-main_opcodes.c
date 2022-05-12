a5b1912ec52d0b1dfb28be7e2da7b7e00704f99f
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-12 14:44:00 +0300

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,1 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
 
+/**
+ * main - prints the opcodes of its own main function
+ *
+ * @argc: number of command line arguments
+ * @argv: An array containing the program command line arguments
+ *
+ * Return: Always 0.
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+return (0);
+}
