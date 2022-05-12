0d529f9d9671f645618502af25ea67c57e0587bf
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-12 17:13:47 +0300

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,1 +1,35 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
 
+/**
+* print_strings - prints strings, followed by a new line.
+*
+* @separator: the string to be printed between strings.
+* @n: number of strings passed to the function.
+*
+* Return: Nothing.
+*/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+	char *str;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(list, char*); /* Get the next value. */
+		if (str)
+			printf("%s", str);
+		else
+			printf("(nil)");
+
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
+
+	va_end(list); /* Clean up argument list. */
+	printf("\n");
+}
