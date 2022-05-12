c76a9de88b8517a1d125d92c1126d649b2736660
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-12 19:17:52 +0300

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,27 +9,51 @@
 void print_all(const char * const format, ...)
 {
 	va_list list;
-	int j = 0;
+	int j = 0, start = 0;
 	char *str;
 
 	va_start(list, format);
 	while (format[j])
 	{
 		if (j != 0)
-			printf(", ");
+			
 
 		switch (format[j])
 		{
 		case 'c':
+			switch (start)
+			{
+				case 1:
+				printf(", ");
+			}
+			start = 1;
 			printf("%c", va_arg(list, int));
 			break;
 		case 'i':
+			switch (start)
+			{
+				case 1:
+				printf(", ");
+			}
+			start = 1;
 			printf("%i", va_arg(list, int));
 			break;
 		case 'f':
+			switch (start)
+			{
+				case 1:
+				printf(", ");
+			}
+			start = 1;
 			printf("%f", va_arg(list, double));
 			break;
 		case's':
+			switch (start)
+			{
+				case 1:
+				printf(", ");
+			}
+			start = 1;
 			str = va_arg(list, char*);
 			if (str)
 				printf("%s", str);
