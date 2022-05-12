c1c80224f3800499216babeac9e033c9544cad6f
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-12 14:58:36 +0300

    Create variadic_functions.h

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef FILE_VARIADIC_FUNCTIONS
+#define FILE_VARIADIC_FUNCTIONS
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
