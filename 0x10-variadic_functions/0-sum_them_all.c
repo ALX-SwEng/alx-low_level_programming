823bf57287c77a82bbd0ef403dee2b15f910da72
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-12 16:04:47 +0300

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,16 +13,16 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list list;
-	int i, sum, count;
+	int i, sum;
 
 	if (n == 0)
 		return (0);
 
-	va_start(list, count); /* Initialize the argument list. */
+	va_start(list, n); /* Initialize the argument list. */
 
 	sum = 0;
 
-	for (i = 0; i < count; i++)
+	for (i = 0; i < n; i++)
 		sum += va_arg(list, int);    /* Get the next argument value. */
 
 	va_end(list); /* Clean up argument list. */
