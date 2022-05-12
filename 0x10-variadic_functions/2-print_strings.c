389cffae0b447a4b2e7526e36a6267bd2aaf998a
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-12 16:37:43 +0300

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,11 +22,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 		for (i = 0; i < n - 1; i++)
 		{
-			printf("%d", va_arg(list, int)); /* Get & print the next argument value. */
-			if (separator)
+			if (va_arg(list, int))
+				printf("%s", va_arg(list, int)); /* Get & print the next value. */
+			else
+				printf("(nil)");
+
+			if (separator && i + 1 < n)
 				printf("%s", separator);
 		}
-		printf("%d", va_arg(list, int)); /* print last argument value */
 
 		va_end(list); /* Clean up argument list. */
 	}
