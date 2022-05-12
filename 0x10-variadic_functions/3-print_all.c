4dabdb6e4cc56d056a6c00c2fe4c6f6344224466
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-12 22:06:12 +0300

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,7 +6,6 @@ void print_char(va_list arg);
 void print_int(va_list arg);
 void print_float(va_list arg);
 void print_string(va_list arg);
-void print_all(const char * const format, ...);
 
 /**
  * print_char - Prints a char.
