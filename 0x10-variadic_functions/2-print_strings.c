369f3a7af16423bc925cca34eb35bece51fd5c28
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-12 16:43:31 +0300

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		va_start(list, n); /* Initialize the argument list. */
 
-		for (i = 0; i < n - 1; i++)
+		for (i = 0; i < n; i++)
 		{
 			if (va_arg(list, int))
 				printf("%s", va_arg(list, char*)); /* Get & print the next value. */
