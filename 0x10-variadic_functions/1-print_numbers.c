e56130fa0856cf48a942580a7aa9c240bbce0acd
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-12 16:08:19 +0300

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,1 +1,30 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
 
+/**
+* sum_them_all - compute sum of all its parameters.
+*
+* @n: number of parameters passed to function.
+*
+* Return: sum, fail otherwise.
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(list, n); /* Initialize the argument list. */
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(list, int);    /* Get the next argument value. */
+
+	va_end(list); /* Clean up argument list. */
+
+return (sum);
+}
