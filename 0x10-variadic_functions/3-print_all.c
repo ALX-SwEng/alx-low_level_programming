507bca89b13d0a89c60651c81740c507d679b336
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-13 05:15:01 +0300

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,12 +8,11 @@
 */
 void print_all(const char * const format, ...)
 {
+	va_list list;
 	int j = 0;
 	char *str, *sep = "";
 
-	va_list list;
 	va_start(list, format);
-
 	if (format)
 	{
 		while (format[j])
