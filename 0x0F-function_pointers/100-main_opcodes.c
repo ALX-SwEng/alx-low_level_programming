6d2863e92e41eca0e886be38d4acd0a716521d99
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-13 05:26:32 +0300

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -12,18 +12,33 @@
 
 int main(int argc, char *argv[])
 {
+	int bytes, index;
+	int (*address)(int, char **) = main;
+	unsigned char opcode;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	
-	if (argc < 0)
+
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
 
+	for (index = 0; index < bytes; index++)
+	{
+		opcode = *(unsigned char *)address;
+		printf("%.2x", opcode);
+
+		if (index == bytes - 1)
+			continue;
+		printf(" ");
+		address++;
+	}
 	
 
 return (0);
