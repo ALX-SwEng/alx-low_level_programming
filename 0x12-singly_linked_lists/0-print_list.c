72e976a4734c49b2c4a41ac836417dafbe29d13e
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-19 19:35:37 +0300

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,14 +15,12 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		printf("[%d]", h->len);
 		if (h->str == NULL)
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		else
-			printf("%s\n", h->str);
+			printf("[%d] %s\n", h->len, h->str);
 
 		++counter;
-
 		h = h->next;
 	}
 
