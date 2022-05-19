64aa5e3ee1e07f64fa85f5bbf79ca5b1d8ea2eb1
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-19 23:21:08 +0300

    Create 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * first - prints a sentence before the main function is executed.
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
