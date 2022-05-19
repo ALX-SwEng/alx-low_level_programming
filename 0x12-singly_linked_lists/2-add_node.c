a68bc45244d7da5d0159d4452a4c0eabaa7b48d6
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-19 21:27:26 +0300

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,7 +11,7 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_s *new_node; /* Create a new node */
+	struct list_s *new_node; /* Create a new node */
 	unsigned int c = 0;
 
 	while (str[c++])
