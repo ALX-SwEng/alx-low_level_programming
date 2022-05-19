3bde794f88b5442305b3178640b06a4f634d6cb5
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-19 19:04:18 +0300

    0x12-singly_linked_lists Commit

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,14 @@
+#ifndef FILE_LISTS
+#define FILE_LISTS
+
+#include <stdlib.h>
+
+int _putchar(char c);
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
