9c00d689b5a1c749482cdeb7cdde6259fc923674
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-19 22:47:14 +0300

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include "lists.h"
+
+/**
+* free_list - frees a singly linked list.
+* @head: a linked list to print
+*
+* Return: void.
+*/
+
+void free_list(list_t *head)
+{
+	list_t *tmp_node; /* Create a temporary node */
+	
+	while ((*head)->next)
+	{
+		tmp_node = *head;
+		*head = (*head)->next;
+		free(tmp_node);
+	}
+}
