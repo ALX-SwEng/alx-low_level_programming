6b7a4b9ab8f83e3cd6521453248a16da939accfa
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-19 21:44:09 +0300

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,41 @@
+#include <string.h>
+#include "lists.h"
+
+/**
+* add_node_end - adds a new node at the end of a singly linked list.
+* @head: a linked list to print
+* @str: needs to be duplicated.
+*
+* Return: the address of the new element, or NULL if it failed.
+*/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node; /* Create a new node */
+	unsigned int c = 0;
+
+	while (str[c++])
+		;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = --c;
+
+	new_node->next = NULL; /* Point it to null */
+  
+  /* if head is NULL, it is an empty list */
+  if(*head == NULL)
+    *head = newNode;
+  else
+  {
+    while(*head->next != NULL)
+    {
+      *head = *head->next;
+    }
+	(*head) = new_node;	/* Point head to new node */
+
+	return (*head);
+}
