5ec30149cd2f84aac11fcf32a5d246d51b1c5ab2
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-19 19:05:16 +0300

    0x12-singly_linked_lists commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,22 @@
 
 #include <stdlib.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+
 int _putchar(char c);
 
 size_t print_list(const list_t *h);
