7acf7de4a78e8a50211b4ea70e6caf6e7ec18bce
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-19 23:51:57 +0300

    0x12-singly_linked_lists commit

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,9 @@
-msg:    db "Hello, Holberton\n", 0
-fmt:    db "%s", 10, 0
-
-        SECTION .text
-        extern printf
-        global main, _start
+          global    main
+          extern    printf
 main:
-_start:
-        mov esi, msg    ; 64-bit ABI passing order starts w/ edi, esi, ...
-        mov edi, fmt    ;
-        mov eax, 0      ; printf is varargs, so EAX counts # of non-integer arguments being passed
-        call printf
-
-        mov ebx, 0      ; normal-exit code
-        mov eax, 1      ; process-termination service
-        int 0x80        ; linux kernel service
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
