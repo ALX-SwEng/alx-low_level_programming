964c5128e7e88784da4185747793c232668fe19d
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-23 12:39:02 +0300

    Update main.c

--- 0x13-more_singly_linked_lists/main.c
+++ 0x13-more_singly_linked_lists/main.c
@@ -5,7 +5,7 @@
 
 /**
  * main - check the code
- * 
+ *
  * Return: Always 0.
  */
 int main(void)
@@ -25,7 +25,7 @@ int main(void)
     new->n = 9;
     new->next = head;
     head = new;
-    n = print_listint(head);
+    n = listint_len(head);
     printf("-> %lu elements\n", n);
     free(new);
     return (0);
