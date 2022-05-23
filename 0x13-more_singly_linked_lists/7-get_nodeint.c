2d4fc0380129ea97a432c42ba4c2e666cafa01b2
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-23 13:20:33 +0300

    0x13. C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* get_nodeint_at_index - find the nth node of a listint_t linked list.
+* @h: a linked list to print
+* @index: the index of the node, starting at 0.
+*
+* Return: the nth node, NULL otherwise.
+*/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	while (head != NULL)
+	{
+		if (head->n == index)
+      return (head);
+    head = head->next;
+	}
+
+return (NULL);
+}
