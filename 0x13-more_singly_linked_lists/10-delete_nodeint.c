33429b7b6757f26e15ee7bdf78fd06eed845084a
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-23 14:16:55 +0300

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,7 +20,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	for (i = 0; i < index - 1; i++)
 	{
-		*head = *head->next;
+		*head = (*head)->next;
 		if (*head == NULL)
 			return (-1);
 	}
