6171e730ebd3c1ed96013b3727f482ecf32c7192
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-23 13:52:39 +0300

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-  listint_t *new_node;
+	listint_t *new_node;
 
 	if (head == NULL)
 		return (NULL);
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (head == NULL)
 			return (NULL);
 	}
-	
+
 	new_node->n = n;
 	new_node->next = (*head)->next;
 	(*head)->next = new_node;
