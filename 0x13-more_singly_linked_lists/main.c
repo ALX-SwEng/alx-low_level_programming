dac469a36db96c9f0acb69ba21dc442e601bbf46
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-23 12:40:24 +0300

    Update main.c

--- 0x13-more_singly_linked_lists/main.c
+++ 0x13-more_singly_linked_lists/main.c
@@ -11,22 +11,16 @@
 int main(void)
 {
     listint_t *head;
-    listint_t *new;
-    listint_t hello = {8, NULL};
-    size_t n;
 
-    head = &hello;
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    new->n = 9;
-    new->next = head;
-    head = new;
-    n = listint_len(head);
-    printf("-> %lu elements\n", n);
-    free(new);
+    head = NULL;
+    add_nodeint(&head, 0);
+    add_nodeint(&head, 1);
+    add_nodeint(&head, 2);
+    add_nodeint(&head, 3);
+    add_nodeint(&head, 4);
+    add_nodeint(&head, 98);
+    add_nodeint(&head, 402);
+    add_nodeint(&head, 1024);
+    print_listint(head);
     return (0);
 }
