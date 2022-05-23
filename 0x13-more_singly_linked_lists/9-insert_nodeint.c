89b484cbce2d98020b331a8a95b7fe982df6f586
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-23 13:52:07 +0300

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -5,7 +5,7 @@
 /**
 * insert_nodeint_at_index - insert new node at the nth node of a linked list.
 * @head: a linked list to print
-* @index: the position where new node added.
+* @idx: the position where new node added.
 * @n: value to be added.
 *
 * Return: the nth node, NULL otherwise.
@@ -21,14 +21,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	for (i = 0; i < idx; i++)
 	{
-    head = head->next;
-    if (head == NULL)
+		head = head->next;
+		if (head == NULL)
 			return (NULL);
 	}
-
-  new_node->n = n;
-	new_node->next = (*head)->next; 
-  (*head)->next = new_node;
+	
+	new_node->n = n;
+	new_node->next = (*head)->next;
+	(*head)->next = new_node;
 
 return (head);
 }
