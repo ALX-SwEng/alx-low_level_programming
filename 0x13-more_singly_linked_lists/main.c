a80b26280ff87aec3d5c759752565f556301693b
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-23 13:41:12 +0300

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/main.c
+++ 0x13-more_singly_linked_lists/main.c
@@ -11,7 +11,7 @@
 int main(void)
 {
     listint_t *head;
-    listint_t *node;
+    int sum;
 
     head = NULL;
     add_nodeint_end(&head, 0);
@@ -22,10 +22,8 @@ int main(void)
     add_nodeint_end(&head, 98);
     add_nodeint_end(&head, 402);
     add_nodeint_end(&head, 1024);
-    print_listint(head);
-    node = get_nodeint_at_index(head, 5);
-    printf("%d\n", node->n);
-    print_listint(head);
+    sum = sum_listint(head);
+    printf("sum = %d\n", sum);
     free_listint2(&head);
     return (0);
 }
