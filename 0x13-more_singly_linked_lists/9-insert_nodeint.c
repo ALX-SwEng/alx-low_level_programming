1a382a86667363b3df9ce4048609dbdb886fc100
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-23 13:57:08 +0300

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,6 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 	listint_t *new_node;
 
+	new_node = malloc(sizeof(listint_t));
 	if (head == NULL)
 		return (NULL);
 
@@ -29,6 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = (*head)->next;
 	(*head)->next = new_node;
+	free(new_node);
 
 return (*head);
 }
