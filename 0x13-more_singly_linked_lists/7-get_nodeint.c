560a4ed23c323f250754ab139dc9a50126211087
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-23 13:35:59 +0300

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -17,7 +17,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 
-	for (i = 0; yi < index; i++)
+	for (i = 0; i < index; i++)
 	{
 		head = head->next;
 		if (head == NULL)
