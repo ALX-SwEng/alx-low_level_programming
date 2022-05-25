8af977dc91b6f14cc56cd76480ab90db8df983df
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-25 21:35:36 +0300

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,9 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
 
-size_t looped_listint_len(const listint_t *head);
-size_t print_listint_safe(const listint_t *head);
-
 /**
  * looped_listint_len - Counts the number of unique nodes
  *                      in a looped listint_t linked list.
