7f6eefa52ebb7370c5537816b4446192eccad3a8
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-25 12:56:23 +0300

    More Exercise on Singly Linked List

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -18,7 +18,7 @@ size_t print_listint_safe(const listint_t *head)
 		++nodes;
 		printf("[%p] %d\n", (void *)head, head->n);
 
-		if (head != head->next)
+		if (head - head->next > 0)
 			head = head->next;
 		else
 		{
