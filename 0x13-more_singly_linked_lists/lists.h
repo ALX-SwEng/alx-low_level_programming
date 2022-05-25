5da99a969519cf30bfbf2fa77f93e816b6973ca5
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-25 21:36:18 +0300

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -35,4 +35,7 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 
+
+size_t looped_listint_len(const listint_t *head);
+
 #endif
