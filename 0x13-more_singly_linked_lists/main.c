28966610acc79ca6b764ebb3b1fac823ed95a075
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-25 19:31:23 +0300

    Update main.c

--- 0x13-more_singly_linked_lists/main.c
+++ 0x13-more_singly_linked_lists/main.c
@@ -34,5 +34,8 @@ int main(void)
     add_nodeint(&head, 402);
     add_nodeint(&head, 1024);
     print_listint_safe(head);
+    free_listint_safe(&head2);
+    free_listint_safe(&head);
+    printf("%p, %p\n", (void *)head2, (void *)head);
     return (0);
 }
