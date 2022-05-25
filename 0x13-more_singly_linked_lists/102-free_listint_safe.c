99c4abffe1c868e808ff09c1890bcf5f784f2fd1
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-25 19:28:13 +0300

    Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,20 +1,23 @@
 #include "lists.h"
 
-size_t looped_listint_count(listint_t *head);
+size_t loop__checker_listint(listint_t *head);
 size_t free_listint_safe(listint_t **h);
 
 /**
- * looped_listint_count - Counts the number of unique nodes
- *                      in a looped listint_t linked list.
+ * loop__checker_listint - check if there is loop in a linked list and
+ *            counts the number of unique nodes in a looped linked list
+ *
  * @head: A pointer to the head of the listint_t to check.
  *
- * Return: If the list is not looped - 0.
+ * Return: 0 - if the list is not looped,
  *         Otherwise - the number of unique nodes in the list.
  */
-size_t looped_listint_count(listint_t *head)
+ 
+size_t loop__checker_listint(listint_t *head)
 {
-	listint_t *tortoise, *hare;
-	size_t nodes = 1;
+	listint_t *tortoise = head;
+	listint_t *hare = head->next;
+	size_t nodes = 0;
 
 	if (head == NULL || head->next == NULL)
 		return (0);
@@ -22,33 +25,26 @@ size_t looped_listint_count(listint_t *head)
 	tortoise = head->next;
 	hare = (head->next)->next;
 
-	while (hare)
+	while (tortoise && hare && hare->next)
 	{
 		if (tortoise == hare)
-		{
-			tortoise = head;
-			while (tortoise != hare)
-			{
-				nodes++;
-				tortoise = tortoise->next;
-				hare = hare->next;
-			}
-
-			tortoise = tortoise->next;
-			while (tortoise != hare)
-			{
-				nodes++;
-				tortoise = tortoise->next;
-			}
-
-			return (nodes);
-		}
-
+			break;
+		
 		tortoise = tortoise->next;
 		hare = (hare->next)->next;
 	}
-
-	return (0);
+	
+	if if (tortoise == hare)
+	{
+		tortoise = head;
+		while (tortoise != hare)
+		{
+			nodes++;
+			tortoise = tortoise->next;
+			hare = hare->next;
+		}
+	}
+return (nodes);
 }
 
 /**
@@ -66,7 +62,7 @@ size_t free_listint_safe(listint_t **h)
 	listint_t *tmp;
 	size_t nodes, index;
 
-	nodes = looped_listint_count(*h);
+	nodes = loop__checker_listint(*h);
 
 	if (nodes == 0)
 	{
