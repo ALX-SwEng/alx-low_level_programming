5029424e8e72529a1a223e9bfb5caf1d5423fcbe
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-25 11:00:12 +0300

    More Exercise on Singly Linked List

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint_safe - print a linked list with a loop
+ * @head: pointer to the first node in the linked list
+ *
+ * Return: pointer to the 1st node in the new list, fail otherwise
+ */
+        
+size_t print_listint_safe(const listint_t *head)
+{
+	listint_t *fast = *head;
+	listint_t *slow = *head;
+	int nodes = 0;
+	
+	while (slow != null && fast != null && fast.next != null)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+    printf("%d\n", slow->n);
+    ++nodes;
+		if (slow == fast)
+		{
+			loop = 1;
+			break;
+		}
+	}
+
+		return (nodes);
+}
