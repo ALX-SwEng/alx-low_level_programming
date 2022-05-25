239379a1ff0b6c90b86848510a7fbf1e26f483b6
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-25 22:32:03 +0300

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -34,7 +34,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
-
+listint_t *find_listint_loop(listint_t *head)
 
 size_t check_looped_listint(const listint_t *head);
 size_t check_looped_listfree(listint_t *head);
