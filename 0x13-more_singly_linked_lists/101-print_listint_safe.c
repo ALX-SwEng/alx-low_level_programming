7f3a530a2ed060ffda94a8a5bcc7b4bfb00f50fe
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-25 21:07:07 +0300

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,26 +5,22 @@
 /**
  * loop__checker_listint - check if there is loop in a linked list and
  *            counts the number of unique nodes in a looped linked list
- *
  * @head: A pointer to the head of the listint_t to check.
  *
  * Return: 0 - if the list is not looped,
  *         Otherwise - the number of unique nodes in the list.
  */
- 
+
 size_t loop__checker_list(const listint_t *head)
 {
-	const listint_t *tortoise;
-	const listint_t *hare;
+	const listint_t *tortoise  = head->next;
+	const listint_t *hare  = head->next->next;
 	size_t nodes = 1;
 
 	if (head == NULL || head->next == NULL)
 		return (0);
 
-	/* check if there is a loop */
-	tortoise = head->next;
-	hare = head->next->next;
-	while (hare)
+	while (hare) /* check if there is a loop */
 	{
 		if (tortoise == hare)
 			break;
@@ -32,22 +28,17 @@ size_t loop__checker_list(const listint_t *head)
 		tortoise = tortoise->next;
 		hare = (hare->next)->next;
 	}
-
-	/* count unique nodes if the list is looped */
-	if (tortoise == hare)
+	if (tortoise == hare) /* count unique nodes if the list is looped */
 	{
-		/* count the noodes till the point of loop */
 		tortoise = head;
-		while (tortoise != hare)
+		while (tortoise != hare) /* count the noodes till the point of loop */
 		{
 			nodes++;
 			tortoise = tortoise->next;
 			hare = hare->next;
 		}
-
-		/* count the noodes after the point of loop */
 		tortoise = tortoise->next;
-		while (tortoise != hare)
+		while (tortoise != hare) /* count the noodes after the point of loop */
 		{
 			nodes++;
 			tortoise = tortoise->next;
