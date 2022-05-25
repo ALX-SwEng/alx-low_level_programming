ff43ccc561fde2aac48711e1260edf0524067bea
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-25 10:08:54 +0300

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,1 +1,27 @@
+#include "lists.h"
+
+/**
+* free_listint_safe - frees a singly linked list.
+* @head: a linked list to print
+*
+* Return: the size of the list that was free’d.
+*/
+
+size_t free_listint_safe(listint_t **h)
+{
+  size_t count_node = 0;
+	listint_t *tmp_node; /* Create a temporary node */
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		tmp_node = (*head)->next;
+		free(*head);
+		(*head) = tmp_node;
+    ++count_node;
+	}
+  return (count_node);
+}
 
