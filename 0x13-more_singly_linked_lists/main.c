0c68e8dfb025b0f58d5696fd4ba9d5cceb60026b
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-25 09:59:44 +0300

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/main.c
+++ 0x13-more_singly_linked_lists/main.c
@@ -5,28 +5,25 @@
 
 /**
  * main - check the code
- * 
+ *
  * Return: Always 0.
  */
 int main(void)
 {
     listint_t *head;
-    listint_t *new;
-    listint_t hello = {8, NULL};
-    size_t n;
 
-    head = &hello;
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    new->n = 9;
-    new->next = head;
-    head = new;
-    n = print_listint(head);
-    printf("-> %lu elements\n", n);
-    free(new);
+    head = NULL;
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+    add_nodeint_end(&head, 4);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 402);
+    add_nodeint_end(&head, 1024);
+    print_listint(head);
+    reverse_listint(&head);
+    print_listint(head);    
+    free_listint2(&head);
     return (0);
 }
