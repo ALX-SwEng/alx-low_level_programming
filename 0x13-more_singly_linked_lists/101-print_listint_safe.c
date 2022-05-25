4e6777260b96e2f11816ad3b4b15bc8bb4c06984
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-25 22:43:25 +0300

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,54 +2,6 @@
 #include <stdio.h>
 
 /**
- * check_looped_listint - check if there is loop in a linked list and
- *            counts the number of unique nodes in a looped linked list
- * @head: A pointer to the head of the listint_t to check.
- *
- * Return: 0 - if the list is not looped,
- *         Otherwise - the number of unique nodes in the list.
- */
-
-size_t check_looped_listint(const listint_t *head)
-{
-	const listint_t *tortoise, *hare;
-	size_t nodes = 1;
-
-	if (head == NULL || head->next == NULL)
-		return (0);
-
-	tortoise = head->next;
-	hare = (head->next)->next;
-
-	while (hare) /* check if there is loop in the list */
-	{
-		if (tortoise == hare)
-			break;
-		tortoise = tortoise->next;
-		hare = (hare->next)->next;
-	}
-
-	if (tortoise == hare) /* count unique node in a looped list */
-	{
-		tortoise = head;
-		while (tortoise != hare)
-		{
-			nodes++;
-			tortoise = tortoise->next;
-			hare = hare->next;
-		}
-		tortoise = tortoise->next;
-		while (tortoise != hare)
-		{
-			nodes++;
-			tortoise = tortoise->next;
-		}
-		return (nodes);
-	}
-
-return (0);
-}
-
  * print_listint_safe - Prints a listint_t list safely.
  * @head: A pointer to the head of the listint_t list.
  *
