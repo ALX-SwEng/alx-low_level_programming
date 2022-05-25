5d6b6d0414bf7831b18fc9c90815397e0655c34f
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-25 13:10:42 +0300

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -4,7 +4,7 @@
 
 /**
  * free_listint_safe - free a linked list with a loop
- * @head: pointer to the first node in the linked list
+ * @h: pointer to the first node in the linked list
  *
  * Return: pointer to the 1st node in the new list, fail otherwise
  */
@@ -12,17 +12,17 @@
 size_t free_listint_safe(listint_t **h)
 {
 	int nodes = 0;
-  listint_t *tmp_node;
+	listint_t *tmp_node;
 
-	while (*head)
+	while (*h)
 	{
 		++nodes;
-		if (*head - (*head)->next > 0)
-    {
-      tmp_node = (*head)->next;
-      free(*head);
-      (*head) = tmp_node;
-    }
+		if (*h - (*h)->next > 0)
+		{
+			tmp_node = (*h)->next;
+			free(*h);
+			(*h) = tmp_node;
+		}
 		else
 			break;
 	}
