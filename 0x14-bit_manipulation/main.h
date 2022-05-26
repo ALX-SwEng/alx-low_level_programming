e6d58692c017afab1de7f2bfc9f9a27a48d2687f
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-27 00:32:00 +0300

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -14,4 +14,6 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
+int _pow_recursion(int x, int y);
+
 #endif /* MAIN_H */
