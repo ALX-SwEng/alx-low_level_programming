4db8f088ead7cbc7ef424c034adb62a459f11710
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-27 01:00:40 +0300

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,1 +1,20 @@
-#include "main"
+#include "main.h"
+
+/**
+ * get_bit -  find the value of a bit at a given index.
+ * @n: decimal number
+ * @index: index starting from 0 of the bit you want to get
+ *
+ * Return: value of a bit
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit;
+
+	bit = (n >> index);
+	if (index > 32)
+	return (-1);
+	return (bit & 1);
+}
+
