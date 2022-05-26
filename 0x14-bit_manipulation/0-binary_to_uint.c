d2e11847ff63e8733fe8e2926144bf19a6e53d6b
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-27 00:16:18 +0300

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,1 +1,31 @@
 #include "main"
+
+#include "main"
+
+/**
+* binary_to_uint - converts a binary number to unsigned int
+*
+* @b: string containing the binary number
+*
+* Return: the decimal number
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int decimal = 0;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; ++i)
+	{
+		if (b[i] != '0' || b[i] != '1')
+			return (0);
+	}
+
+	while (--i >= 0)
+		decimal += (b[i] * pow(2, i));
+		
+	return (decimal);	
+}
