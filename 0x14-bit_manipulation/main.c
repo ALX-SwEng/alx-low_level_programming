84cbc72d1b05d8a34e5758fc6def0bb0e129780d
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-30 01:09:32 +0300

    Update main.c

--- 0x14-bit_manipulation/main.c
+++ 0x14-bit_manipulation/main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <limits.h>
 #include "main.h"
 
 /**
@@ -9,23 +8,13 @@
  */
 int main(void)
 {
-    print_binary(0);
-    printf("\n");
-    print_binary(1);
-    printf("\n");
-    print_binary(2);
-    printf("\n");
-    print_binary(10);
-    printf("\n");
-    print_binary(954372342312);
-	printf("\n");
-	print_binary(ULONG_MAX);
-	printf("\n");
-    print_binary(98);
-    printf("\n");
-    print_binary(1024);
-    printf("\n");
-    print_binary((1 << 10) + 1);
-    printf("\n");
+    int n;
+
+    n = get_bit(1024, 10);
+    printf("%d\n", n);
+    n = get_bit(98, 1);
+    printf("%d\n", n);
+    n = get_bit(1024, 0);
+    printf("%d\n", n);
     return (0);
 }
