96fcef4fb0adcb1a6bc46d14e105255d88725e6f
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-30 01:52:17 +0300

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,1 +1,35 @@
 #include "main"
+
+/**
+ * flip_bits - find number of bits you would need to flip
+ *  to get the 2nd number from the first number to another
+ *
+ * @n: 1st number
+ * @m: 2nd number (destiny)
+ *
+ * Return: # of bits to flip
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int count = 0;
+	unsigned long int current = n;
+
+	while (current)
+	{
+		current = current >> 1;
+		count++;
+	}
+
+	if (!count)
+		putchar('0');
+
+	while (count)
+	{
+		current = n >> --count;
+		if (current & 1)
+			putchar('1');
+		else
+			putchar('0');
+	}
+}
