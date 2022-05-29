fcd9df75df1fb173e3b08951f476c1173fd01c41
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-30 02:55:48 +0300

    0x14-bit_manipulation

--- 0x14-bit_manipulation/main.c
+++ 0x14-bit_manipulation/main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <limits.h>
 #include "main.h"
 
 /**
@@ -9,15 +8,15 @@
  */
 int main(void)
 {
-    int n;
+    unsigned int n;
 
-    n = get_bit(1024, 10);
-    printf("%d\n", n);
-    n = get_bit(98, 1);
-    printf("%d\n", n);
-    n = get_bit(1024, 0);
-    printf("%d\n", n);
-    n = get_bit(2048, 20);
-    printf("%d\n", n);
+    n = flip_bits(1024, 1);
+    printf("%u\n", n);
+    n = flip_bits(402, 98);
+    printf("%u\n", n);
+    n = flip_bits(1024, 3);
+    printf("%u\n", n);
+    n = flip_bits(1024, 1025);
+    printf("%u\n", n);
     return (0);
 }
