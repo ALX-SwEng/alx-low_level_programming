f18b241e971ba9f3d775e8a4a8c80053f7bb99da
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-30 01:40:09 +0300

    x14-bit_manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_bit -  find the value of a bit at a given index.
+ * set_bit -  find the value of a bit at a given index.
  * @n: decimal number
  * @index: index starting from 0 of the bit you want to get
  *
@@ -10,9 +10,13 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 32)
+	if (index > 32) /* because size of long int is 32 bits */
 		return (-1);
 
-  *n = *n | (1 << index); /* shift left 1 by index bits */
-	return (1);
+	 /* First shift left 1 by index bits 
+	 * and then OR/Add it the given number
+	 */
+	*n = *n | (1 << index);
+
+return (1);
 }
