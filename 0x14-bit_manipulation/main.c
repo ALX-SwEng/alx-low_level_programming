e409f02c63f84c6699b35d243fbb22c062e68e17
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-30 01:14:43 +0300

    Update main.c

--- 0x14-bit_manipulation/main.c
+++ 0x14-bit_manipulation/main.c
@@ -16,5 +16,13 @@ int main(void)
     printf("%d\n", n);
     n = get_bit(1024, 0);
     printf("%d\n", n);
+	n = get_bit(2048, 11);
+	printf("%d\n", n);
+    n = get_bit(2048, 1);
+	printf("%d\n", n);
+    n = get_bit(2048, 20);
+	printf("%d\n", n);
+  	n = get_bit(ULONG_MAX, 0);
+	printf("%d\n", n);
     return (0);
 }
