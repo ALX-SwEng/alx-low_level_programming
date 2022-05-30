3f2a646a955d24fd289bacf308d0ff07117c9472
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-30 20:21:47 +0300

    0x15-file_io header file commit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,13 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <elf.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int _putchar(char c);
 
