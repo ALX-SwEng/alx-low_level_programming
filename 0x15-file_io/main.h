7246c89a45e72980577406a6fa569b4048d1d27a
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-30 23:30:12 +0300

    0x15-file_io header file commit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -17,4 +17,17 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void print_error (int file_des);
 
+/**
+ * ELF header functions
+*/
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+
 #endif
