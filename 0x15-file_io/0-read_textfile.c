4b81ef00a34ef881e19b01d62ad0cdc0c050900a
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-30 20:30:54 +0300

    0x15-file_io tak-0 commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int rc, wc; /* read & write character counter */
 	int fp;  /* File pointer */
 
-	fp = fopen(filename, O_RDONLY);
+	fp = open(filename, O_RDONLY);
 	if (filename == NULL || fp < 0)
 		return (0);
 
