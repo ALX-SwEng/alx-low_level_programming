a392de5b5f3a788a50e7353b5975e8b189f6e32d
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-30 21:11:18 +0300

    0x15-file_io main.c commit

--- 0x15-file_io/main.c
+++ 0x15-file_io/main.c
@@ -9,16 +9,14 @@
  */
 int main(int ac, char **av)
 {
-    ssize_t n;
+    int res;
 
-    if (ac != 2)
+    if (ac != 3)
     {
-        dprintf(2, "Usage: %s filename\n", av[0]);
+        dprintf(2, "Usage: %s filename text\n", av[0]);
         exit(1);
     }
-    n = read_textfile(av[1], 114);
-    printf("\n(printed chars: %li)\n", n);
-    n = read_textfile(av[1], 1024);
-    printf("\n(printed chars: %li)\n", n);
+    res = create_file(av[1], av[2]);
+    printf("-> %i)\n", res);
     return (0);
 }
