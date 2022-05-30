dc5ad790faf902f7a2bd0e875b3058e67f0e0d99
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-30 20:39:11 +0300

    0x15-file_io tak-0 commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,44 @@
 #include "main.h"
+
 /**
- * read_textfile - reads a text file and prints it to the standard output
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ *
  * @filename: name of the file to be read
  * @letters: number of letters to read and print
+ *
  * Return: the number of letters printed, or 0 if it failed
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int i, y;
-	char *buf;
+	char *buffer;
+	int rc, wc; /* read & write character counter */
+	int fp;  /* File pointer */
+
 	if (!filename)
 		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+
+	fp = open(filename, O_RDONLY);
+	if (fp < 0)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
+
+	buffer = malloc(letters * sizeof(char));
+	if (buffer == NULL)
 		return (0);
-	i = read(fd, buf, letters);
-	if (i < 0)
+
+	rc = read(fp, buffer, letters);
+	if (rc < 0)
 	{
-		free(buf);
+		free(buffer);
 		return (0);
 	}
-	buf[i] = '\0';
-	close(fd);
-	y = write(STDOUT_FILENO, buf, i);
-	if (y < 0)
-	{
-		free(buf);
+	buffer[rc] = '\0';
+
+	wc = write(STDOUT_FILENO, buffer, rc);
+	free(buffer);
+
+	if (wc < 0)		
 		return (0);
-	}
-	free(buf);
-	return (y);
+
+return (wc);
 }
