980c597c0c4a9f2f139bfee45a2b0c2ed6e32310
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-30 21:24:55 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,35 +1,33 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * create_file - reads a text file and prints it to the POSIX standard output
- *
- * @filename: name of the file to be read
- * @text_content: a NULL terminated string to write to the file
+ * create_file - creates a file and fills it with text
+ * @filename: name of the file to create
+ * @text_content: text to write in the file
  *
  * Return: 1 on success, -1 on failure
  */
-
 int create_file(const char *filename, char *text_content)
 {
-	int wc1, wc2, fp;
+	int fd, x, y = 0;
 
 	if (!filename)
 		return (-1);
 
-	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fp < 0)
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd < 0)
 		return (-1);
 
 	if (text_content)
 	{
-		while (text_content[wc1])
-			wc1++;
-
-		wc2 = write(fp, text_content, wc1);
-		if (wc2 != wc1)
+		while (text_content[y])
+			y++;
+		x = write(fd, text_content, y);
+		if (x != y)
 			return (-1);
 	}
 
-close(fp);
-return (1);
+	close(fd);
+
+	return (1);
 }
