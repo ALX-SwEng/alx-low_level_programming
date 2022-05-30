5941325cf225211ec88bf1e0a975f5e2e776af5b
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-30 22:10:04 +0300

    0x15-file_io tak-3 commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,16 +1,59 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-* main - prints the number of arguments passed to command line.
+* main - copies the content of a file to another file.
+*
 * @argc: number of command line arguments
 * @argv: An array containing the program command line arguments
 *
 * Return: Always success.
 */
 
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
-	if (argc > 0)
-		printf("%d\n", argc - 1);
+	int fp_from, fp_to, wc;
+	char *buffer = malloc(1024 * sizeof(char));
 
+	if (argc < 3)
+	{
+		dprintf(STDERROR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fp_from = open(argv[1], O_RDONLY);
+	if (*argv[1] == NULL || fp_from < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	fp_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fp_to < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	while ((wc = read(fp_from, buffer, 1024)) > 0)
+	{
+		if (wc != write(fp_to, buffer, 1024))
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fp_from);
+			close(fp_to);
+			exit(99);
+		}
+	}
+
+	if (close(fp_from) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fp_from);
+		exit(100);
+	}
+	if (close(fp_to) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fp_to);
+		exit(100);
+	}
 return (0);
+}
