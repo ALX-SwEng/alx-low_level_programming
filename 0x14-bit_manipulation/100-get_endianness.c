f25ed9007137199ab9c652fde2cffb4f0e588599
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-30 03:13:42 +0300

    0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 - if big endian, 1 - if little endian
+ *
+ * Description: In big endian format the most significant byte
+ *              is stored first, thus gets stored at the 
+ *               smallest address byte, while in little endian format
+ *              the least significant byte is stored first.
+ */
+
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *) &i;
+
+	return (*c);
+}
