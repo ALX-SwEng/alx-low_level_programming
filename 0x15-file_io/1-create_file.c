c52655f23da13d58c2e3e75568ffff2aade36771
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-30 21:21:56 +0300

    0x15-file_io tak-1 commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fp < 0)
 		return (-1);
 
