63af28dc6580e43e1da91ba660a80dea0be5b314
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-30 22:54:53 +0300

    0x15-file_io header file commit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,5 +15,6 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void print_error (int file_des);
 
 #endif
