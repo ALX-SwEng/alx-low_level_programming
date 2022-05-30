2ca02132b8e95d682d9486330e2c88d7e4d83e32
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-30 22:52:05 +0300

    0x15-file_io tak-3 commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,25 +9,24 @@
 int main(int argc, char *argv[])
 {
 	int fp_from, fp_to, wc;
-	char *buffer = malloc(1024 * sizeof(char));
+	char buffer = [1024];
 
-	if (argc < 3)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		exit(97); }
 	fp_from = open(argv[1], O_RDONLY);
 	if (argv[1] == NULL || fp_from < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		exit(98); }
+
 	fp_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fp_to < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		close(fp_from);
+		exit(99); }
 	while ((wc = read(fp_from, buffer, 1024)) > 0)
 	{
 		if (wc != write(fp_to, buffer, 1024))
@@ -37,13 +36,27 @@ int main(int argc, char *argv[])
 			close(fp_to);
 			exit(99);
 	}}
-	if (close(fp_from) < 0 || close(fp_to) < 0)
+	if (wc < 0)
 	{
-		if (close(fp_from) < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fp_from);
-		if (close(fp_to) < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fp_to);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98); }
+
+	if (close(fp_from) < 0)
+		print_error(fp_from);
+	if (close(fp_to) < 0)
+		print_error(fp_to);	
 return (0);
+}
+
+/**
+* print_error - print file close error.
+* @file_des: file descriptor
+*
+* Return: None.
+*/
+
+void print_error (int file_des)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_des);
+	exit(100);
 }
