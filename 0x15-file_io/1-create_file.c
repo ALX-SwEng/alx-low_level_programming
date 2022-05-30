ea299afff232ed903bd11b7051b9f2d00a33dae9
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-30 21:09:52 +0300

    0x15-file_io tak-1 commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,7 +6,7 @@
  * @filename: name of the file to be read
  * @text_content: a NULL terminated string to write to the file
  *
- * Return: 1 on success, -1 on failure 
+ * Return: 1 on success, -1 on failure
  */
 
 int create_file(const char *filename, char *text_content)
