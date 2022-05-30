8ff6a43422ce32a3f6695c47cbfa0134e24849c5
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-30 20:07:55 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,1 +1,43 @@
+#include "main.h"
 
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ *
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ *
+ * Return: the number of letters printed, or 0 if it failed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+  char *buffer;
+  int read_c, write_c;
+  FILE *fp;
+ 
+  fp = fopen (*filename, O_RDONLY);
+  
+  if (filename == NULL || fp < 0)
+    return (0);
+  
+  buffer = malloc(letters * sizeof(char));
+  if (buffer == NULL)
+    return (0);
+  
+  read_c = read(fp, buffer, letters);
+  if (read_c < 0)
+  {
+    free(buffer);
+    return (0);
+  }
+  buffer[read_c] = '\0';
+  
+   write_c = write(STDOUT_FILENO, buffer, read_c);
+  if (write_c < 0)
+  {
+    free(buffer);
+    return (0);
+  }
+ free(buffer);
+  return (write_c);
+}
