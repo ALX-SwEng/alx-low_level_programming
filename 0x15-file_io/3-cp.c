46883fb30a89cc90a68724f3ccf4bf2fe64a7355
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-30 21:32:53 +0300

    0x15-file_io tak-3 commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+/**
+* main - prints the number of arguments passed to command line.
+* @argc: number of command line arguments
+* @argv: An array containing the program command line arguments
+*
+* Return: Always success.
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	if (argc > 0)
+		printf("%d\n", argc - 1);
+
+return (0);
