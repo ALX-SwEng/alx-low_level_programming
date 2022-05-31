a3e4cd213c352b8c12f72fa29b32b0b1232b6a4f
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-31 21:38:22 +0300

    0x15-file_io tak-4 commit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -17,9 +17,6 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void print_error(int file_des);
 
-/**
- * ELF header functions
-*/
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
