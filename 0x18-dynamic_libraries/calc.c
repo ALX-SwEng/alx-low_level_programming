f190ca4ba3513380b00b0e5d860f001beed673d3
root <root@DESKTOP-OSDVMDS.localdomain>
2022-07-11 05:20:57 +0300

    0x18. C - Dynamic libraries project commit

---
+++ 0x18-dynamic_libraries/calc.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+/**
+ * add - compute sum of two numbers
+ * @a: the 1st number to be added
+ * @b: the 2nd number to be added
+ * Return: sum of two numbers.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - compute difference of two numbers
+ * @a: the 1st number to be added
+ * @b: the 2nd number to be added
+ * Return: sum of two numbers.
+ */
+int sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * mul - compute product of two numbers
+ * @a: the 1st number to be added
+ * @b: the 2nd number to be added
+ * Return: sum of two numbers.
+ */
+int mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * mul - compute quetiont of two numbers
+ * @a: the 1st number to be added
+ * @b: the 2nd number to be added
+ * Return: sum of two numbers.
+ */
+int div(int a, int b)
+{
+	 return (a / b);
+}
+
+/**
+ * mul - compute module of two numbers
+ * @a: the 1st number to be added
+ * @b: the 2nd number to be added
+ * Return: sum of two numbers.
+ */
+int mod(int a, int b)
+{
+         return (a % b);
+}
+
